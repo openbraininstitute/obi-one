2d210ce5730e1acebd4044d79d89dd023b105354
James Isbister <isbisterjb@gmail.com>
2025-02-10 13:31:30 +0100

    Git ignore fix

--- OBI API/build/lib/obi/__init__.py
+++
@@ -1,8 +0,0 @@
-from .multi_template import MultiTemplate
-from .circuit import Circuit
-from .circuit_grouping import CircuitGrouping, NeuronCircuitGrouping
-from .timestamps import Timestamps
-from .recording import Recording
-from .simulation import Simulation
-from .simulation_campaign import SimulationCampaign
-from .stimulus import Stimulus, SynchronousSingleSpikeStimulus
--- OBI API/build/lib/obi/events.py
+++
@@ -1,6 +0,0 @@
-from .multi_template import MultiTemplate
-
-class Events(MultiTemplate):
-    start_time: float | list[float]
-    repetitions: int | list[int]
-    iti: float | list[float]
