40b966a5817e963cd183e48c1fc97d8460807d96
James Isbister <isbisterjb@gmail.com>
2025-02-10 15:01:04 +0100

    build removed

--- obi/build/lib/obi/circuit_grouping.py
+++
@@ -1,8 +0,0 @@
-from .multi_template import MultiTemplate
-from .circuit import Circuit
-
-class CircuitGrouping(MultiTemplate):
-    circuit: Circuit
-
-class NeuronCircuitGrouping(CircuitGrouping):
-    neuron_ids: tuple[int, ...] | list[tuple[int, ...]]
