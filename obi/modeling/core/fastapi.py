8d966de7c982abe18ebd2c4ec02a44e6cb1f62c2
James Isbister <isbisterjb@gmail.com>
2025-03-04 09:56:04 +0100

    Ongoing changes

--- obi/modeling/core/fastapi.py
+++ obi/modeling/core/fastapi.py
@@ -7,16 +7,24 @@ db: Dict[str, Dict[int, Dict[str, Any]]] = {}
 def activate_fastapi_app(app: FastAPI):
 
     for subclass in Form.__subclasses__():
-        print(subclass)
         generate_routes(subclass, app)
-        return
+    return
 
 # Auto-generate API routes
 def generate_routes(model: Type[Form], app: FastAPI):
 
+
+    # Model is the OBI.Form subclass 
+    # i.e. <class 'obi.modeling.simulation.simulations.SimulationsForm'>
     print(model)
 
+
+    # model_name is the name of the model (i.e. OBI.Form subclass) 
+    # in lowercase (i.e. 'simulationsform')
     model_name = model.__name__.lower()
+    print(model_name)
+
+    # Create a new dictionary in the db dictionary with the key as the model_name
     if model_name not in db:
         db[model_name] = {}
 
@@ -26,6 +34,14 @@ def generate_routes(model: Type[Form], app: FastAPI):
         # db[model_name][item.id] = item.dict()
         # print(item)
         return item
+
+
+    @app.get(f"/{model_name}/pythonic_schema/{{item_id}}", response_model=model)
+    async def get_pythonic_schema(item: model):
+        """Get pythonic schema"""
+        return item.get_pythonic_schema()
+
+
     
     @app.get(f"/{model_name}/generate/{{item_id}}", response_model=model)
     async def call_generate(item_id: int):
