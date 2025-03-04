c17522b9df1019a82e21796016249895276d0adb
James Isbister <isbisterjb@gmail.com>
2025-03-04 14:02:12 +0100

    Working fastapi changes

--- examples/call_service_example.py
+++ examples/call_service_example.py
@@ -1,11 +1,157 @@
 import requests
 
-params = {
-	'name': "test",
-	'id': 1
-}
+# params = {
+# 	'name': "test",
+# 	'id': 1
+# }
 # print(requests.post("http://127.0.0.1:8000/circuitsimulation/", json=params).json())
 # print(requests.get("http://127.0.0.1:8000/circuitsimulation/1").json())
 # print(requests.get("http://127.0.0.1:8000/circuitsimulation/generate/1").json())
 
-print(requests.get("http://127.0.0.1:8000/simulationsform/generate/1").json()) # 
+# print(requests.get("http://127.0.0.1:8000/simulationsform/generate/1").json()) # 
+
+
+params = {
+  "timestamps": {
+    "additionalProp1": {
+      "start_time": 0
+    },
+    "additionalProp2": {
+      "start_time": 0
+    },
+    "additionalProp3": {
+      "start_time": 0
+    }
+  },
+  "stimuli": {
+    "additionalProp1": {
+      "synapse_set": {
+        "circuit": {
+          "circuit_path": "string",
+          "node_set": "string"
+        }
+      },
+      "timestamps": {
+        "start_time": 0
+      }
+    },
+    "additionalProp2": {
+      "synapse_set": {
+        "circuit": {
+          "circuit_path": "string",
+          "node_set": "string"
+        }
+      },
+      "timestamps": {
+        "start_time": 0
+      }
+    },
+    "additionalProp3": {
+      "synapse_set": {
+        "circuit": {
+          "circuit_path": "string",
+          "node_set": "string"
+        }
+      },
+      "timestamps": {
+        "start_time": 0
+      }
+    }
+  },
+  "recordings": {
+    "additionalProp1": {
+      "start_time": 0,
+      "end_time": 0
+    },
+    "additionalProp2": {
+      "start_time": 0,
+      "end_time": 0
+    },
+    "additionalProp3": {
+      "start_time": 0,
+      "end_time": 0
+    }
+  },
+  "neuron_sets": {
+    "additionalProp1": {
+      "circuit": {
+        "circuit_path": "string",
+        "node_set": "string"
+      }
+    },
+    "additionalProp2": {
+      "circuit": {
+        "circuit_path": "string",
+        "node_set": "string"
+      }
+    },
+    "additionalProp3": {
+      "circuit": {
+        "circuit_path": "string",
+        "node_set": "string"
+      }
+    }
+  },
+  "synapse_sets": {
+    "additionalProp1": {
+      "circuit": {
+        "circuit_path": "string",
+        "node_set": "string"
+      }
+    },
+    "additionalProp2": {
+      "circuit": {
+        "circuit_path": "string",
+        "node_set": "string"
+      }
+    },
+    "additionalProp3": {
+      "circuit": {
+        "circuit_path": "string",
+        "node_set": "string"
+      }
+    }
+  },
+  "intracellular_location_sets": {
+    "additionalProp1": {
+      "neuron_ids": [
+        0
+      ],
+      "section": "string"
+    },
+    "additionalProp2": {
+      "neuron_ids": [
+        0
+      ],
+      "section": "string"
+    },
+    "additionalProp3": {
+      "neuron_ids": [
+        0
+      ],
+      "section": "string"
+    }
+  },
+  "extracellular_location_sets": {
+    "additionalProp1": {},
+    "additionalProp2": {},
+    "additionalProp3": {}
+  },
+  "initialize": {
+    "circuit": {
+      "circuit_path": "string",
+      "node_set": "string"
+    },
+    "simulation_length": 100,
+    "random_seed": 1,
+    "extracellular_calcium_concentration": 1.1,
+    "v_init": -80,
+    "sonata_version": 1,
+    "target_simulator": "CORENEURON",
+    "timestep": 0.025
+  }
+}
+
+
+
+print(requests.post("http://127.0.0.1:8000/simulationsform/create_form/", json=params).json())
--- obi/modeling/core/fastapi.py
+++ obi/modeling/core/fastapi.py
@@ -1,59 +1,81 @@
 from fastapi import FastAPI, HTTPException
 from typing import Type, Dict, Any, List
-from obi.modeling.core.form import Form
+# from obi.modeling.core.form import Form
+import obi
 
-db: Dict[str, Dict[int, Dict[str, Any]]] = {}
+# db: Dict[str, Dict[int, Dict[str, Any]]] = {}
+
+current_form: obi.Form = None
 
 def activate_fastapi_app(app: FastAPI):
 
-    for subclass in Form.__subclasses__():
+    for subclass in obi.Form.__subclasses__():
         generate_routes(subclass, app)
     return
 
 # Auto-generate API routes
-def generate_routes(model: Type[Form], app: FastAPI):
-
+def generate_routes(model: Type[obi.Form], app: FastAPI):
 
     # Model is the OBI.Form subclass 
     # i.e. <class 'obi.modeling.simulation.simulations.SimulationsForm'>
     print(model)
-
 
     # model_name is the name of the model (i.e. OBI.Form subclass) 
     # in lowercase (i.e. 'simulationsform')
     model_name = model.__name__.lower()
     print(model_name)
 
-    # Create a new dictionary in the db dictionary with the key as the model_name
-    if model_name not in db:
-        db[model_name] = {}
+    # Create a new dictionary in the db dictionary with the key as the lowercase model_name
+    # if model_name not in db:
+    #     db[model_name] = {}
 
-    @app.post(f"/{model_name}/", response_model=model)
-    async def create_item(item: model):
+    @app.post(f"/{model_name}/create_form/", response_model=model)
+    async def create_form(item: model):
         """Create an item"""
-        # db[model_name][item.id] = item.dict()
-        # print(item)
+
+        global current_form
+        current_form = item
         return item
 
+    @app.get(f"/{model_name}/schema/")
+    async def get_schema():
+        """Get schema of the model"""
+        return model.schema()
 
-    @app.get(f"/{model_name}/pythonic_schema/{{item_id}}", response_model=model)
-    async def get_pythonic_schema(item: model):
-        """Get pythonic schema"""
-        return item.get_pythonic_schema()
+    @app.get(f"/{model_name}/generate_grid_scan/", response_model=obi.GridScan)
+    async def generate_grid_scan():
+        """Call generate method on an item by ID"""
+        
+        grid_scan = obi.GridScan(form=current_form, output_root='../../obi_output/fastapi_test/circuit_simulations/grid_scan')
+        grid_scan.generate()
+
+        return grid_scan
+
+
+            # if item_id not in db[model_name]:
+        #     raise HTTPException(status_code=404, detail="Item not found")
+        # item_data = db[model_name][item_id]
+
+        # item = model(**item_data)
+        # print(item.__class__)
+
+
+    # @app.get(f"/{model_name}/obi_class_serialization/")
+    # async def get_obi_class_serialization():
+    #     """Get obi class serialization"""
+    #     return model.serialize()
+
+    # @app.get(f"/{model_name}/pythonic_schema/") # , response_model=model
+    # async def get_pythonic_schema():
+    #     """Get pythonic schema"""
+    #     # print(model)
+    #     return model.get_pythonic_schema()
+    #     # model.get_pythonic_schema()
+    #     # return item.get_pythonic_schema()
 
 
     
-    @app.get(f"/{model_name}/generate/{{item_id}}", response_model=model)
-    async def call_generate(item_id: int):
-        """Call generate method on an item by ID"""
-        # if item_id not in db[model_name]:
-        #     raise HTTPException(status_code=404, detail="Item not found")
-        item_data = db[model_name][item_id]
 
-        item = model(**item_data)
-        print(item.__class__)
-        print(item.generate())
-        return item
 
 
     # # @app.get(f"/{model_name}/{{item_id}}", response_model=model)
