23fab26dff32e8708d0f711b52efaccdd89de69a
Christoph Pokorny <christoph.pokorny@openbraininstitute.org>
2025-03-13 16:12:53 +0100

    Excluded subclasses with single coordinates from forms union

--- obi/modeling/unions/unions.py
+++ obi/modeling/unions/unions.py
@@ -1,8 +1,10 @@
 from typing import Union, Type
 from obi.modeling.core.base import OBIBaseModel
+from obi.modeling.core.single import SingleCoordinateMixin
 
 def get_all_subclasses(cls):
     subclasses = set(cls.__subclasses__())
+    subclasses = set(filter(lambda _cls: SingleCoordinateMixin not in _cls.__bases__, subclasses))  # Don't include subclasses with single coordinates
     for subclass in subclasses.copy():
         subclasses.update(get_all_subclasses(subclass))
     return subclasses
