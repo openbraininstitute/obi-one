9eb5b8c9c9834bd306d4750ebf86408eee15074e
James Isbister <isbisterjb@gmail.com>
2025-03-19 16:28:30 +0100

    Temporary customization of circuit_extraction.py for launch

--- obi/modeling/circuit_extraction/circuit_extraction.py
+++ obi/modeling/circuit_extraction/circuit_extraction.py
@@ -25,6 +25,14 @@ from brainbuilder.utils.sonata import split_population
 from importlib.metadata import version
 
 
+import os
+import json
+def copy_directory(source_dir, destination_dir):
+
+    if os.path.exists(source_dir):
+
+        os.makedirs(destination_dir, exist_ok=True)
+        os.system(f"cp -r {source_dir} {destination_dir}")
 
 class CircuitExtraction(CircuitExtractions, SingleCoordinateMixin):
     """"""
@@ -39,6 +47,35 @@ class CircuitExtraction(CircuitExtractions, SingleCoordinateMixin):
                                             self.initialize.circuit_path,
                                             True,
                                             False)
+            
+
+            with open(self.coordinate_output_root + "circuit_config.json", 'r') as config_file:
+                config = json.load(config_file)
+
+                for nodes_dict in config['networks']['nodes']:
+                    for population_key, population_dict in nodes_dict['populations'].items():
+
+                        # if 'morphologies_dir' in population_dict:                
+                        #     print("Copying morphologies")
+                            # copy_directory(population_dict['morphologies_dir'], os.path.join(self.coordinate_output_root, "morphologies"))
+                        if 'biophysical_neuron_models_dir' in population_dict:
+                            print("Copying biophysical_neuron_models")
+                            copy_directory(population_dict['biophysical_neuron_models_dir'], os.path.join(self.coordinate_output_root, "emodels_hoc"))
+
+            with open(self.initialize.circuit_path, 'r') as original_circuit_config_file:
+                original_circuit_config = json.load(original_circuit_config_file)
+
+                original_circuit_config_copy = original_circuit_config.copy()
+                original_circuit_config_copy['networks']['edges'][0]['edges_file'] = "external_S1nonbarrel_neurons__S1nonbarrel_neurons__chemical/external_S1nonbarrel_neurons__S1nonbarrel_neurons__chemical.h5"
+
+                with open(self.coordinate_output_root + "/circuit_config.json", 'w') as config_file:
+                    json.dump(original_circuit_config_copy, config_file, indent=4)
+
+
+
+            
+
+
         except Exception as e:
             if str(e) == "Unable to synchronously create group (name already exists)":
                 print("Error:", f"Subcircuit {self.initialize.node_set} already exists. Subcircuit must be deleted before running the extraction.")
