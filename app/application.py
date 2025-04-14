d10831739a923ff552a7c53d2412f02ef0f259f4
Gianluca Ficarelli <26835404+GianlucaFicarelli@users.noreply.github.com>
2025-04-14 16:17:04 +0200

    Add pydantic-config and fix CORS (#87)

--- app/application.py
+++ app/application.py
@@ -1,32 +1,21 @@
-# Launch server
-# uvicorn examples.launch_service_example:app --reload
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import obi
-from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-from starlette.middleware.cors import CORSMiddleware
+from app.config import settings
 
-app = FastAPI(title="obi-one")
-
-# Define allowed origins
-origins = [
-    "http://localhost:3000",  # Allow frontend running on React/Vue/Next.js
-    # "http://127.0.0.1:3000"
-    # "*"  # Allow all domains (not recommended for production)
-]
-
-# Trying to fix CORS issue (currently not solved)
+app = FastAPI(
+    title=settings.APP_NAME,
+    version=settings.APP_VERSION or "0.0.0",
+    debug=settings.APP_DEBUG,
+    root_path=settings.ROOT_PATH,
+)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=settings.CORS_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["Access-Control-Allow-Origin"]
 )
-
-# Possibly useful to solving issue
-# app.headers["Access-Control-Allow-Origin"] = True
-
 
 obi.activate_fastapi_app(app)
