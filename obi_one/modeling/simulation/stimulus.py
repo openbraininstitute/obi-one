f8d307b3b00d01b12b37732bc46780b1be520a1b
James Isbister <isbisterjb@gmail.com>
2025-04-15 21:53:37 +0100

    NoiseCurrentClampSomaticStimulus and PercentageNoiseCurrentClampSomaticStimulus added

--- obi_one/modeling/simulation/stimulus.py
+++ obi_one/modeling/simulation/stimulus.py
@@ -6,9 +6,6 @@ from obi_one.modeling.unions.unions_timestamps import TimestampsUnion
 from obi_one.modeling.unions.unions_synapse_set import SynapseSetUnion
 from obi_one.modeling.unions.unions_neuron_sets import NeuronSetUnion
 
-# Repeated descriptions
-REPRESENTS_PHYSICAL_ELECTRODE_DESCRIPTION = "Default is False. If True, the signal will be implemented using a NEURON IClamp mechanism. The IClamp produce an electrode current which is not included in the calculation of extracellular signals, so this option should be used to represent a physical electrode. If the noise signal represents synaptic input, represents_physical_electrode should be set to False, in which case the signal will be implemented using a MembraneCurrentSource mechanism, which is identical to IClamp, but produce a membrane current, which is included in the calculation of the extracellular signal."
-
 
 class Stimulus(Block, ABC):
     name: str = Field(description="Name of the stimulus.")
@@ -25,6 +22,7 @@ class SomaticStimulus(Stimulus, ABC):
     delay: float | list[float] = Field(default=0.0, title='Delay', description="Time in ms when input is activated.")
     duration: float | list[float] = Field(default=1.0, title='Duration', description="Time duration in ms for how long input is activated.")
     neuron_set: NeuronSetUnion | list[NeuronSetUnion] = Field(description="Neuron set to which the stimulus is applied.")
+    represents_physical_electrode: bool = Field(default=False, description="Default is False. If True, the signal will be implemented using a NEURON IClamp mechanism. The IClamp produce an electrode current which is not included in the calculation of extracellular signals, so this option should be used to represent a physical electrode. If the noise signal represents synaptic input, represents_physical_electrode should be set to False, in which case the signal will be implemented using a MembraneCurrentSource mechanism, which is identical to IClamp, but produce a membrane current, which is included in the calculation of the extracellular signal.")
 
 
 class ConstantCurrentClampSomaticStimulus(SomaticStimulus):
@@ -32,7 +30,6 @@ class ConstantCurrentClampSomaticStimulus(SomaticStimulus):
     _input_type: str = "current_clamp"
 
     amplitude: float | list[float] = Field(default=0.1, description="The injected current. Given in nA.")
-    represents_physical_electrode: bool | list[bool] = Field(default=False, description=REPRESENTS_PHYSICAL_ELECTRODE_DESCRIPTION)
 
     def generate(self):
 
@@ -58,7 +55,6 @@ class LinearCurrentClampSomaticStimulus(SomaticStimulus):
 
     amplitude_start: float | list[float] = Field(default=0.1, description="The amount of current initially injected when the stimulus activates. Given in nA.")
     amplitude_end: float | list[float] = Field(default=0.2, description="If given, current is interpolated such that current reaches this value when the stimulus concludes. Otherwise, current stays at amp_start. Given in nA")
-    represents_physical_electrode: bool | list[bool] = Field(default=False, description=REPRESENTS_PHYSICAL_ELECTRODE_DESCRIPTION)
 
     def generate(self):
         
@@ -84,7 +80,6 @@ class RelativeConstantCurrentClampSomaticStimulus(SomaticStimulus):
     _input_type: str = "current_clamp"
 
     percentage_of_threshold_current: float | list[float] = Field(default=10.0, description="The percentage of a cell’s threshold current to inject when the stimulus activates.")
-    represents_physical_electrode: bool | list[bool] = Field(default=False, description=REPRESENTS_PHYSICAL_ELECTRODE_DESCRIPTION)
 
     def generate(self):
         
@@ -110,7 +105,6 @@ class RelativeLinearCurrentClampSomaticStimulus(SomaticStimulus):
 
     percentage_of_threshold_current_start: float | list[float] = Field(default=10.0, description="The percentage of a cell’s threshold current to inject when the stimulus activates.")
     percentage_of_threshold_current_end: float | list[float] = Field(default=100.0, description="If given, the percentage of a cell’s threshold current is interpolated such that the percentage reaches this value when the stimulus concludes.")
-    represents_physical_electrode: bool | list[bool] = Field(default=False, description=REPRESENTS_PHYSICAL_ELECTRODE_DESCRIPTION)
 
     def generate(self):
         
@@ -138,7 +132,6 @@ class MultiPulseCurrentClampSomaticStimulus(SomaticStimulus):
     amplitude: float | list[float] = Field(default=0.1, description="The amount of current initially injected when each pulse activates. Given in nA.")
     width: float | list[float] = Field(default=1.0, description="The length of time each pulse lasts. Given in ms.")
     frequency: float | list[float] = Field(default=1.0, description="The frequency of pulse trains. Given in Hz.")
-    represents_physical_electrode: bool | list[bool] = Field(default=False, description=REPRESENTS_PHYSICAL_ELECTRODE_DESCRIPTION)
 
     def generate(self):
         
@@ -167,7 +160,6 @@ class SinusoidalCurrentClampSomaticStimulus(SomaticStimulus):
     peak_amplitude: float | list[float] = Field(default=0.1, description="The peak amplitude of the sinusoid. Given in nA.")
     frequency: float | list[float] = Field(default=1.0, description="The frequency of the waveform. Given in Hz.")
     dt: float | list[float] = Field(default=0.025, description="Timestep of generated signal in ms. Default is 0.025 ms.")
-    represents_physical_electrode: bool | list[bool] = Field(default=False, description=REPRESENTS_PHYSICAL_ELECTRODE_DESCRIPTION)
 
     def generate(self):
         
@@ -194,7 +186,6 @@ class SubthresholdCurrentClampSomaticStimulus(SomaticStimulus):
     _input_type: str = "current_clamp"
 
     percentage_below_threshold: float | list[float] = Field(default=0.1, description="A percentage adjusted from 100 of a cell’s threshold current. E.g. 20 will apply 80\% of the threshold current. Using a negative value will give more than 100. E.g. -20 will inject 120\% of the threshold current.")
-    represents_physical_electrode: bool | list[bool] = Field(default=False, description=REPRESENTS_PHYSICAL_ELECTRODE_DESCRIPTION)
 
     def generate(self):
         
@@ -221,8 +212,6 @@ class HyperpolarizingCurrentClampSomaticStimulus(SomaticStimulus):
     _module: str = "hyperpolarizing"
     _input_type: str = "current_clamp"
 
-    represents_physical_electrode: bool | list[bool] = Field(default=False, description=REPRESENTS_PHYSICAL_ELECTRODE_DESCRIPTION)
-
     def generate(self):
         
         sonata_config = {}
@@ -240,7 +229,58 @@ class HyperpolarizingCurrentClampSomaticStimulus(SomaticStimulus):
         return sonata_config
 
 
+class NoiseCurrentClampSomaticStimulus(SomaticStimulus):
 
+    _module: str = "noise"
+    _input_type: str = "current_clamp"
+
+    mean_amplitude = Field(default=0.01, description="The mean value of current to inject. Given in nA.")
+    variance = Field(default=0.01, description="The variance around the mean of current to inject using a normal distribution.")
+
+    def generate(self):
+
+        sonata_config = {}
+        
+        for t_ind, timestamp in enumerate(self.timestamps.timestamps()):
+            sonata_config[self.name + "_" + str(t_ind)] = 
+                {
+                    "delay": timestamp,
+                    "duration": self.duration,
+                    "cells":  self.neuron_set.name,
+                    "module": self._module,
+                    "input_type": self._input_type,
+                    "mean": self.mean_amplitude,
+                    "variance": self.variance,
+                    "represents_physical_electrode": self.represents_physical_electrode
+                }
+        return sonata_config
+
+
+class PercentageNoiseCurrentClampSomaticStimulus(SomaticStimulus):
+
+    _module: str = "noise"
+    _input_type: str = "current_clamp"
+
+    mean_percentage_of_threshold_current = Field(default=0.01, description="The mean value of current to inject as a percentage of a cell’s threshold current.")
+    variance = Field(default=0.01, description="The variance around the mean of current to inject using a normal distribution.")
+
+    def generate(self):
+
+        sonata_config = {}
+        
+        for t_ind, timestamp in enumerate(self.timestamps.timestamps()):
+            sonata_config[self.name + "_" + str(t_ind)] = 
+                {
+                    "delay": timestamp,
+                    "duration": self.duration,
+                    "cells":  self.neuron_set.name,
+                    "module": self._module,
+                    "input_type": self._input_type,
+                    "mean_percent": self.mean_percentage_of_threshold_current,
+                    "variance": self.variance,
+                    "represents_physical_electrode": self.represents_physical_electrode
+                }
+        return sonata_config
 
 
 class SynchronousSingleSpikeStimulus(Stimulus):
