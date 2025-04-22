211d89ef51720d027db4d41ee1e255bf0fd6597b
James Isbister <isbisterjb@gmail.com>
2025-04-22 21:17:02 +0100

    SingleBlockEntitySDKTestForm added for testing gui with entitysdk

--- obi_one/modeling/test_forms/test_form_single_block.py
+++ obi_one/modeling/test_forms/test_form_single_block.py
@@ -1,7 +1,6 @@
 from obi_one.core.form import Form
 from obi_one.core.block import Block
 from obi_one.core.single import SingleCoordinateMixin
-# from obi_one.core.db import SaveCircuitEntity, SaveCircuitCollectionEntity
 from obi_one.core.path import NamedPath
 
 from obi_one.core.db import *
@@ -9,31 +8,20 @@ from obi_one.core.db import *
 class SingleBlockGenerateTestForm(Form):
     """
     """
-
     _single_coord_class_name: str = "SingleBlockGenerateTest"
 
     class Initialize(Block):
         morphology_path: NamedPath | list[NamedPath]
-        # soma: str | list[str]
 
     initialize: Initialize
 
-    # def save_collection(self, circuit_entities):
-    #     SaveCircuitCollectionEntity(circuits=circuit_entities)
-        
-
 
 import traceback
-import neurom
-import numpy as np
 class SingleBlockGenerateTest(SingleBlockGenerateTestForm, SingleCoordinateMixin):
     
     def run(self):
 
-        try:
-
-            # print(f"Metrics for morphology '{self.initialize.morphology_path}'")
-            
+        try:            
             with open(self.initialize.morphology_path, 'w') as file:
                 file.write("Hello world!")
 
@@ -41,9 +29,22 @@ class SingleBlockGenerateTest(SingleBlockGenerateTestForm, SingleCoordinateMixin
             traceback.print_exception(e)
         
         return
+        
+        
+from obi_one.core.db import *
+class SingleBlockEntitySDKTestForm(Form):
+    """
+    """
+    _single_coord_class_name: str = "SingleBlockGenerateTest"
 
-    # def save_single(self):
-    #     circuit_entity = SaveCircuitEntity(config_path=self.coordinate_output_root + "circuit_config.json")
-    #     return circuit_entity
-        
-        
+    class Initialize(Block):
+        morphology: ReconstructionMorphology | list[ReconstructionMorphology]
+
+    initialize: Initialize
+
+class SingleBlockEntitySDKTest(Form):
+
+    def run(self):
+
+        return
+
