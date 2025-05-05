d2fe2db8931fa584422ad279b4204e7b3e82e977
James Isbister <isbisterjb@gmail.com>
2025-05-05 08:15:09 +0200

    decalred neurom_metrics endpoint includes entity and asset fetch

--- obi_one/endpoints/declared_endpoints.py
+++ obi_one/endpoints/declared_endpoints.py
@@ -12,6 +12,9 @@ import entitysdk.common
 
 from fastapi import APIRouter
 
+from entitysdk.models.morphology import ReconstructionMorphology
+from neurom import load_morphology
+
 def activate_declared_router(router: APIRouter) -> APIRouter:
 
     class ReconstructionMorphologyMetricsOutput(BaseModel):
@@ -26,12 +29,39 @@ def activate_declared_router(router: APIRouter) -> APIRouter:
         L.info("neurom_metrics")
 
         try:
-            output = ReconstructionMorphologyMetricsOutput(
-                soma_radius=self.initialize.morphology.neurom_morphology,
-                soma_surface_area=self.initialize.morphology.neurom_morphology,
-            )
 
-            return output
+            morphology = entity_client.get_entity(
+                            entity_id=reconstruction_morphology_id, entity_type=ReconstructionMorphology, token=client.token
+                        )
+            L.info(f"morphology: {morphology}")
+
+            morphology_path = ""
+            for asset in morphology.assets:
+                if asset['content_type'] == "application/asc":
+
+                    morphology_path = Path(settings.OUTPUT_DIR / "obi-entity-file-store" / asset['full_path'])
+                    print(file_output_path)
+                    morphology_path.parent.mkdir(parents=True, exist_ok=True)
+
+                    client.download_file(
+                        entity_id=morphology.id,
+                        entity_type=entity_type,
+                        asset_id=asset['id'],
+                        output_path=morphology_path,
+                        token=db.token,
+                    )
+
+            if morphology_path == "":
+                L.exception("Generic exception")
+            else:
+                neurom_morphology = load_morphology(morphology_path)
+
+                output = ReconstructionMorphologyMetricsOutput(
+                    soma_radius=neurom.get("soma_radius", neurom_morphology),
+                    soma_surface_area=neurom.get("soma_surface_area", neurom_morphology),
+                )
+
+                return output
 
         except Exception:  # noqa: BLE001
             L.exception("Generic exception")
