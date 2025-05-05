a6c2431199552120d8f524538cada131948c5c15
Gianluca Ficarelli <gianluca.ficarelli@gmail.com>
2025-05-05 11:07:45 +0200

    Add ENTITYCORE_URL and minor fixes

--- app/dependencies/entitysdk.py
+++ app/dependencies/entitysdk.py
@@ -1,14 +1,13 @@
 import entitysdk.client
 import entitysdk.common
-from app.config import settings
+from starlette.requests import Request
 
+from app.config import settings
 from app.dependencies.auth import UserContextDep
 
+
 class FixedTokenManager:
-    """
-    A fixed token manager that always returns the same token.
-    This is used for testing purposes.
-    """
+    """A fixed token manager that always returns the same token."""
 
     def __init__(self, token: str):
         self._token = token
@@ -16,15 +15,23 @@ class FixedTokenManager:
     def get_token(self) -> str:
         return self._token
 
-from starlette.requests import Request
+
 def get_client(
     user_context: UserContextDep,
     request: Request,
-    ) -> entitysdk.client.Client:
-    project_context = entitysdk.common.ProjectContext(project_id=user_context.project_id, virtual_lab_id=user_context.virtual_lab_id)
+) -> entitysdk.client.Client:
+    if user_context.project_id and user_context.virtual_lab_id:
+        project_context = entitysdk.common.ProjectContext(
+            project_id=user_context.project_id,
+            virtual_lab_id=user_context.virtual_lab_id,
+        )
+    else:
+        project_context = None
     token_manager = FixedTokenManager(user_context.token.credentials)
     client = entitysdk.client.Client(
-        api_url=settings.ENTITYCORE_API_URL, project_context=project_context, 
-        http_client=request.state.http_client, token_manager=token_manager, environment=settings.ENVIRONMENT, 
-        )
+        api_url=settings.ENTITYCORE_URL,
+        project_context=project_context,
+        http_client=request.state.http_client,
+        token_manager=token_manager,
+    )
     return client
