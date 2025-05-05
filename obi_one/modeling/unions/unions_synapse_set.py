f4c697214e34ff89956028df67e5a414ea4f7c9c
Christoph Pokorny <christoph.pokorny@openbraininstitute.org>
2025-05-05 15:08:30 +0200

    Fixed merge

--- obi_one/modeling/unions/unions_synapse_set.py
+++ obi_one/modeling/unions/unions_synapse_set.py
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-from obi.modeling.unions.unions import subclass_union
-
-from obi.modeling.circuit.synapse_sets import *
-=======
 from obi_one.modeling.unions.unions import subclass_union
 
 from obi_one.modeling.circuit.synapse_sets import *
->>>>>>> main
 
 SynapseSetUnion = subclass_union(SynapseSet)
