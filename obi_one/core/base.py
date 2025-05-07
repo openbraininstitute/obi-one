8566ec7979359747e33c6014471db0b28967dde5
James Isbister <isbisterjb@gmail.com>
2025-05-07 14:25:55 +0200

    obi_type parameter changed back to type

--- obi_one/core/base.py
+++ obi_one/core/base.py
@@ -9,16 +9,16 @@ from obi_one.core.serialization import entity_encoder
 
 class OBIBaseModel(BaseModel):
     """
-    Sets `obi_type` fields for model_dump which are then used for desserialization.
+    Sets `type` fields for model_dump which are then used for desserialization.
     Sets encoder for EntitySDK Entities
     """
 
-    obi_type: str = ""
+    type: str = ""
     model_config = ConfigDict(json_encoders={Entity: entity_encoder})
 
     @model_validator(mode="before")
     @classmethod
     def set_type(cls, data: Any) -> Any:
-        if isinstance(data, dict) and "obi_type" not in data:
-            data["obi_type"] = cls.__qualname__
+        if isinstance(data, dict) and "type" not in data:
+            data["type"] = cls.__qualname__
         return data
--- obi_one/core/serialization.py
+++ obi_one/core/serialization.py
@@ -7,9 +7,9 @@ def entity_encoder(obj: Any) -> Dict[str, str]:
     """
     cls_name = obj.__class__.__name__
     if issubclass(obj.__class__, Entity) and "FromID" not in cls_name:
-        return {"obi_type": f"{cls_name}FromID", "id_str": str(obj.id)}
+        return {"type": f"{cls_name}FromID", "id_str": str(obj.id)}
     elif "FromID" in cls_name:
-        return {"obi_type": cls_name, "id_str": str(obj.id)}
+        return {"type": cls_name, "id_str": str(obj.id)}
     raise TypeError(f"Object of type {cls_name} is not JSON serializable")
 
 
@@ -17,7 +17,7 @@ import obi_one as obi
 import json
 
 def deserialize_obi_object_from_json_data(json_data):
-    obi_object = getattr(obi, json_data["obi_type"]).model_validate(json_data)
+    obi_object = getattr(obi, json_data["type"]).model_validate(json_data)
     return obi_object
 
 def deserialize_obi_object_from_json_file(json_path):
