9039ea1075e642f75dc244fe2cf65cdd7932667e
James Isbister <isbisterjb@gmail.com>
2025-05-07 17:07:47 +0200

    core/base.py reset to previous functionality for compatability with gui

--- obi_one/core/base.py
+++ obi_one/core/base.py
@@ -1,24 +1,52 @@
-from typing import Any
+from typing import Any, Literal
 from pydantic import (
     BaseModel,
     ConfigDict,
-    model_validator
+    model_validator,
+    Field, 
+    ValidatorFunctionWrapHandler
 )
 from entitysdk.models.entity import Entity
 from obi_one.core.serialization import entity_encoder
+    
 
 class OBIBaseModel(BaseModel):
+
     """
     Sets `type` fields for model_dump which are then used for desserialization.
     Sets encoder for EntitySDK Entities
     """
 
     type: str = ""
-    model_config = ConfigDict(json_encoders={Entity: entity_encoder})
 
     @model_validator(mode="before")
     @classmethod
-    def set_type(cls, data: Any) -> Any:
+    def set_type(cls, data):
+        """Automatically sets `type` when instantiated in Python."""
         if isinstance(data, dict) and "type" not in data:
             data["type"] = cls.__qualname__
         return data
+
+    def __init_subclass__(cls, **kwargs):
+        """Dynamically set the `type` field to the class name"""
+        super().__init_subclass__(**kwargs)
+        cls.__annotations__["type"] = Literal[cls.__qualname__]
+
+    def __str__(self):
+        return self.__repr__()
+
+    @model_validator(mode='wrap')  # noqa  # the decorator position is correct
+    @classmethod
+    def retrieve_type_on_deserialization(cls, value: Any,
+                                         handler: ValidatorFunctionWrapHandler) -> 'OBIBaseModel':
+        if isinstance(value, dict):
+            # WARNING: we do not want to modify `value` which will come from the outer scope
+            # WARNING2: `sub_cls(**modified_value)` will trigger a recursion, and thus we need to remove `obi_class`
+            modified_value = value.copy()
+            sub_cls_name = modified_value.pop('type', None)
+            if sub_cls_name is not None:
+                sub_cls = get_subclass_recursive(cls=OBIBaseModel, name=sub_cls_name, allow_same_class=True)
+                return sub_cls(**modified_value)
+            else:
+                return handler(value)
+        return handler(value)
