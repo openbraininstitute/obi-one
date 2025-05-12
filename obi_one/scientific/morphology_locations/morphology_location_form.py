68f1c393442a8d1a80f4e85570f64663ac2315c9
Michael W. Reimann <michael.reimann@openbraininstitute.org>
2025-05-12 09:18:31 +0200

    Form now plots and dumps locations.

--- obi_one/scientific/morphology_locations/morphology_location_form.py
+++ obi_one/scientific/morphology_locations/morphology_location_form.py
@@ -1,3 +1,7 @@
+import numpy
+import pandas
+import os
+
 from typing import Union, ClassVar, Annotated
 from pydantic import BaseModel, Field
 from neurom.core.types import SectionType
@@ -41,9 +45,38 @@ class MorphologyLocations(MorphologyLocationsForm,SingleCoordinateMixin):
     """
     """
 
+    def _generate_plot(self, m, df):
+        import neurom.view
+        import neurom.io
+        
+        from matplotlib import pyplot as plt
+        from .specified_morphology_locations import _SEC_ID, _SEG_ID, _SEG_OFF, _PRE_IDX
+
+        def location_xyz(row):
+            secid = int(row[_SEC_ID])
+            segid = int(row[_SEG_ID])
+            o = row[_SEG_OFF]
+            seg = m.sections[secid - 1].points[segid:(segid + 2)]
+            dseg = numpy.diff(seg, axis=0)[0]
+            dseg = dseg / numpy.linalg.norm(dseg)
+            return pandas.Series(seg[0] + o * dseg,
+                                index=["x", "y", "z"])
+
+
+        fig = plt.figure(figsize=(3, 6))
+        ax = fig.gca()
+
+        xyz = pandas.concat([df.apply(location_xyz, axis=1), df[_PRE_IDX]], axis=1)
+        neurom.view.plot_morph(neurom.io.utils.Morphology(m), ax=ax)
+        xyz.groupby(_PRE_IDX).apply(lambda _x: ax.scatter(_x["x"], _x["y"], s=6))
+        plt.axis("equal")
+        return fig
+
+
     def run(self):
         
         try:
+            print(self.coordinate_output_root)
             from .specified_morphology_locations import generate_neurite_locations_on
             if isinstance(self.initialize.morphology, Path):
                 import morphio
@@ -61,7 +94,10 @@ class MorphologyLocations(MorphologyLocationsForm,SingleCoordinateMixin):
                 self.initialize.max_dist_from_center,
                 self.initialize.lst_section_types
             )
-            print(df)
+            
+            fig = self._generate_plot(m, df)
+            fig.savefig(os.path.join(self.coordinate_output_root, "locations_plot.pdf"))
+            df.to_csv(os.path.join(self.coordinate_output_root, "morphology_locations.csv"))
 
         except Exception as e:  # noqa: BLE001
             print(f"An error occurred: {e}")
