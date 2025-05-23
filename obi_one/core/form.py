538279805e49c85795eaa9b6bb3d01ca106c8289
James Isbister <isbisterjb@gmail.com>
2025-05-23 06:27:50 +0200

    core/form.py linting complete

--- obi_one/core/form.py
+++ obi_one/core/form.py
@@ -4,13 +4,18 @@ from obi_one.core.base import OBIBaseModel
 
 
 class Form(OBIBaseModel):
-    """ """
+    """A Form is a configuration for single or multi-dimensional parameter scans.
+
+    A Form is composed of Blocks, which either appear at the root level
+    or within dictionaries of Blocks where the dictionary is takes a Union of Block types.
+    """
 
     name: ClassVar[str] = "Add a name class' name variable"
     description: ClassVar[str] = """Add a description to the class' description variable"""
     single_coord_class_name: ClassVar[str] = ""
 
-    def cast_to_single_coord(self):
+    def cast_to_single_coord(self) -> OBIBaseModel:
+        """Cast the form to a single coordinate object."""
         module = __import__(self.__module__)
         class_to_cast_to = getattr(module, self.single_coord_class_name)
         single_coord = class_to_cast_to.model_construct(**self.__dict__)
@@ -18,5 +23,5 @@ class Form(OBIBaseModel):
         return single_coord
 
     @property
-    def single_coord_scan_default_subpath(self):
+    def single_coord_scan_default_subpath(self) -> str:
         return self.single_coord_class_name + "/"
