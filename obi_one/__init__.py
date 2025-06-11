febb0bdc16777351564d4a885dcd6a9f6ea5199e
James Isbister <isbisterjb@gmail.com>
2025-06-11 14:05:45 +0200

    Info Block added, EntityFromID now inherits from OBIBaseModel

--- obi_one/__init__.py
+++ obi_one/__init__.py
@@ -2,6 +2,7 @@ from obi_one.core.base import OBIBaseModel
 from obi_one.core.block import Block
 from obi_one.core.block_reference import BlockReference
 from obi_one.core.form import Form
+from obi_one.core.info import Info
 from obi_one.core.path import NamedPath
 from obi_one.core.scan import CoupledScan, GridScan
 from obi_one.core.serialization import (
@@ -43,6 +44,7 @@ __all__ = [
     "GridScan",
     "HyperpolarizingCurrentClampSomaticStimulus",
     "IDNeuronSet",
+    "Info",
     "IntracellularLocationSet",
     "IntracellularLocationSetUnion",
     "LinearCurrentClampSomaticStimulus",
