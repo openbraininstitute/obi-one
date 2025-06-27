a88e558abf87b13d73772ceedacaa2aea045fed6
james-isbister <isbisterjb@gmail.com>
2025-06-27 14:44:36 +0200

    Schema_update: Form block group order, Info strings (#268)
    * singular_name, group, group_order, 
    * Info strings min length 1, no default

--- obi_one/core/info.py
+++ obi_one/core/info.py
@@ -3,5 +3,5 @@ from obi_one.core.block import Block
 from pydantic import Field
 
 class Info(Block):
-    campaign_name: str = Field(default="No name provided", description="The users name for the simulation")
-    campaign_description: str = Field(default="No description provided", description="Description of the simulation")
+    campaign_name: str = Field(min_length=1, description="The users name for the simulation")
+    campaign_description: str = Field(min_length=1, description="Description of the simulation")
