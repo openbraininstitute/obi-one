864e68574bc3d1681aff241eaa057fcffb0f13cb
Christoph Pokorny <christoph.pokorny@openbraininstitute.org>
2025-06-27 16:20:45 +0200

    Fixed title

--- obi_one/scientific/circuit/neuron_sets.py
+++ obi_one/scientific/circuit/neuron_sets.py
@@ -338,7 +338,7 @@ class ExcitatoryNeurons(PredefinedNeuronSet):
 class InhibitoryNeurons(PredefinedNeuronSet):
     """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."""
 
-    title: ClassVar[str] = "Excitatory Neurons"
+    title: ClassVar[str] = "Inhibitory Neurons"
 
     node_set: ClassVar[str] = "Inhibitory"
 
