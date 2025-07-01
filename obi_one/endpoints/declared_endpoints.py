7d3ac25fe39a5f15ce8f1083bc983388b492ca5b
dkeller9 <daniel.keller@openbraininstitute.org>
2025-07-01 15:44:28 +0200

    Update declared_endpoints.py

--- obi_one/endpoints/declared_endpoints.py
+++ obi_one/endpoints/declared_endpoints.py
@@ -36,6 +36,72 @@ from obi_one.endpoints.validation_config_page import get_validation_config_page_
 # Define the path to your validation config file
 VALIDATION_CONFIG_PATH = Path("validation_config.json")  # Adjust this path if your file is elsewhere
 
+def activate_declared_endpoints(router: APIRouter) -> APIRouter:
+    @router.get(
+        "/neuron-morphology-metrics/{reconstruction_morphology_id}",
+        summary="Neuron morphology metrics",
+        description="This calculates neuron morphology metrics for a given reconstruction \
+                    morphology.",
+    )
+    def neuron_morphology_metrics_endpoint(
+        db_client: Annotated[entitysdk.client.Client, Depends(get_client)],
+        reconstruction_morphology_id: str,
+    ) -> MorphologyMetricsOutput:
+        L.info("get_morphology_metrics")
+
+        try:
+            metrics = get_morphology_metrics(
+                reconstruction_morphology_id=reconstruction_morphology_id,
+                db_client=db_client,
+            )
+        except entitysdk.exception.EntitySDKError:
+            raise HTTPException(
+                status_code=HTTPStatus.NOT_FOUND,
+                detail={
+                    "code": ApiErrorCode.NOT_FOUND,
+                    "detail": (
+                        f"Reconstruction morphology {reconstruction_morphology_id} not found."
+                    ),
+                },
+            )
+
+        if metrics:
+            return metrics
+        L.error(
+            f"Reconstruction morphology {reconstruction_morphology_id} metrics computation issue"
+        )
+        raise ApiError(
+            message="Asset not found",
+            error_code=ApiErrorCode.NOT_FOUND,
+            http_status_code=HTTPStatus.NOT_FOUND,
+        )
+
+    @router.get(
+        "/electrophysiologyrecording-metrics/{trace_id}",
+        summary="electrophysiology recording metrics",
+        description="This calculates electrophysiology traces metrics for a particular recording",
+    )
+    def electrophysiologyrecording_metrics_endpoint(
+        trace_id: str,
+        db_client: Annotated[entitysdk.client.Client, Depends(get_client)],
+        requested_metrics: CALCULATED_FEATURES | None = Query(default=None),
+        amplitude: AmplitudeInput = Depends(),
+        protocols: STIMULI_TYPES | None = Query(default=None),
+    ) -> ElectrophysiologyMetricsOutput:
+        try:
+            ephys_metrics = get_electrophysiology_metrics(
+                trace_id=trace_id,
+                entity_client=db_client,
+                calculated_feature=requested_metrics,
+                amplitude=amplitude,
+                stimuli_types=protocols,
+            )
+            return ephys_metrics
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Internal Server Error: {e}")
+
+    return router
+'''
 #we will later import this from entitycore directly
 #but entitycore has no PyPi package currently
 class EntityType(StrEnum):
@@ -235,3 +301,4 @@ def activate_declared_endpoints(router: APIRouter) -> APIRouter:
 
 
     return router
+'''
