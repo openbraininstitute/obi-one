023c98249ef977668a88d1bb3b599f1d7abf711e
dkeller9 <daniel.keller@openbraininstitute.org>
2025-07-07 09:58:31 +0200

    Update declared_endpoints.py

--- obi_one/endpoints/declared_endpoints.py
+++ obi_one/endpoints/declared_endpoints.py
@@ -1,4 +1,4 @@
-from enum import Enum, auto, StrEnum
+from enum import Enum, auto, StrEnum 
 from fastapi import APIRouter, Depends, HTTPException, Query, Request
 from fastapi.responses import HTMLResponse, JSONResponse
 import entitysdk.client
