f9960e24943c363b04c8b5395b9ea5bdae58ae87
Christoph Pokorny <christoph.pokorny@openbraininstitute.org>
2025-07-16 18:12:45 +0200

    Fixed linting in circuits.py

--- obi_one/scientific/circuit/circuit.py
+++ obi_one/scientific/circuit/circuit.py
@@ -6,13 +6,17 @@ from obi_one.core.base import OBIBaseModel
 
 
 class Circuit(OBIBaseModel):
-    """Class representing a circuit, i.e., pointing to a SONATA config and possible additional assets."""
+    """Class representing a circuit.
+
+    It points to a SONATA config and possible additional assets.
+    """
 
     name: str
     path: str
     matrix_path: str | None = None
 
-    def __init__(self, name, path, **kwargs) -> None:
+    def __init__(self, name: str, path: str, **kwargs) -> None:
+        """Initialize object."""
         super().__init__(name=name, path=path, **kwargs)
         c = snap.Circuit(self.path)  # Basic check: Try to load the SONATA circuit w/o error
 
@@ -22,19 +26,23 @@ class Circuit(OBIBaseModel):
             )  # Basic check: Try to load the connectivity matrix w/o error
             np.testing.assert_array_equal(
                 cmat.vertices["node_ids"], c.nodes[self._default_population_name(c)].ids()
-            )  # TODO: This assumes the connectivity matrix is the local one, might need to be extended in the future.
+            )  # TODO: This assumes the conn. mat. is the local one; to be extended in the future.
 
     def __str__(self) -> str:
+        """Returns the name as a string representation."""
         return self.name
 
     @property
-    def sonata_circuit(self):
+    def sonata_circuit(self) -> snap.Circuit:
         """Provide access to SONATA circuit object."""
         return snap.Circuit(self.path)
 
     @property
-    def connectivity_matrix(self):
-        """Provide access to corresponding ConnectivityMatrix object. In case of a multi-graph, returns the compressed version."""
+    def connectivity_matrix(self) -> ConnectivityMatrix:
+        """Provide access to corresponding ConnectivityMatrix object.
+
+        Note: In case of a multi-graph, returns the compressed version.
+        """
         if self.matrix_path is None:
             msg = "Connectivity matrix has not been found"
             raise FileNotFoundError(msg)
@@ -44,12 +52,12 @@ class Circuit(OBIBaseModel):
         return cmat
 
     @property
-    def node_sets(self):
+    def node_sets(self) -> list:
         """Returns list of available node sets."""
         return list(self.sonata_circuit.node_sets.content.keys())
 
     @staticmethod
-    def get_node_population_names(c: snap.Circuit, incl_virtual=True):
+    def get_node_population_names(c: snap.Circuit, *, incl_virtual: bool = True) -> list:
         """Returns node population names."""
         popul_names = c.nodes.population_names
         if not incl_virtual:
@@ -57,18 +65,20 @@ class Circuit(OBIBaseModel):
         return popul_names
 
     @staticmethod
-    def _default_population_name(c: snap.Circuit):
+    def _default_population_name(c: snap.Circuit) -> str:
         """Returns the default node population name of a SONATA circuit c."""
         popul_names = Circuit.get_node_population_names(c, incl_virtual=False)
-        assert len(popul_names) == 1, "Default node population unknown!"
+        if len(popul_names) != 1:
+            msg = "Default node population unknown!"
+            raise ValueError(msg)
         return popul_names[0]
 
     @property
-    def default_population_name(self):
+    def default_population_name(self) -> str:
         """Returns the default node population name."""
         return self._default_population_name(self.sonata_circuit)
 
-    def get_edge_population_names(self, incl_virtual=True):
+    def get_edge_population_names(self, *, incl_virtual: bool = True) -> list:
         """Returns edge population names."""
         popul_names = self.sonata_circuit.edges.population_names
         if not incl_virtual:
