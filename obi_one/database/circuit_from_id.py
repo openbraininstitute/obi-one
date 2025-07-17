c611a192af402fea904130442f5aca335a24b207
Christoph Pokorny <christoph.pokorny@openbraininstitute.org>
2025-07-17 14:07:39 +0200

    Fixed linting in circuit_from_id.py

--- obi_one/database/circuit_from_id.py
+++ obi_one/database/circuit_from_id.py
@@ -15,15 +15,14 @@ class CircuitFromID(EntityFromID):
     _entity: Circuit | None = PrivateAttr(default=None)
 
     def download_circuit_directory(
-        self, dest_dir=Path(), db_client: entitysdk.client.Client = None
+        self, dest_dir: Path = Path(), db_client: entitysdk.client.Client = None
     ) -> None:
         for asset in self.entity(db_client=db_client).assets:
             if asset.label == "sonata_circuit":
                 circuit_dir = dest_dir / asset.path
                 if circuit_dir.exists():
-                    raise FileExistsError(
-                        f"Circuit directory '{circuit_dir}' already exists and is not empty."
-                    )
+                    msg = f"Circuit directory '{circuit_dir}' already exists and is not empty."
+                    raise FileExistsError(msg)
 
                 circuit = db_client.get_entity(entity_id=self.id_str, entity_type=Circuit)
                 stage_circuit(
