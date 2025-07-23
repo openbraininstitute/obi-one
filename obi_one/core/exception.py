c25ec8ae1376f635edbf4731bfe5c2a8f9cbae5e
ilkilic <10600022+ilkilic@users.noreply.github.com>
2025-07-23 17:55:41 +0200

    Ephys agent: Handle missing protocols in data with explicit error (#295)
    * update example notebook
    
    * handle no protocols found in the data
    
    * add unit-tests
    
    * update bpe version + update python requirement >=3.12.2
    
    * uv lock
    
    * try to use stimuli protocol from the trace metadata
    
    * update unit tests
    
    * filter and warn on invalid stimulus types, raise error only if none are valid
    
    * minor update

--- obi_one/core/exception.py
+++ obi_one/core/exception.py
@@ -4,3 +4,8 @@ class OBIONE_Error(Exception):
 
 class ConfigValidationError(OBIONE_Error):
     """Exception raised for validation errors in OBI-ONE."""
+
+class ProtocolNotFoundError(Exception):
+    def __init__(self, msg: list[str]):
+        message = (msg)
+        super().__init__(message)
