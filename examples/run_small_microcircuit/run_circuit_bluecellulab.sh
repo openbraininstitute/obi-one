10d635f04fdcc7e77ea6c8511c0a88504ce84452
ilkankilic <ilkan.kilic@openbraininstitute.org>
2025-08-06 12:17:08 +0200

    remove tiny_circuits/N_10__top_nodes_dim6__asc

--- examples/run_small_microcircuit/run_circuit_bluecellulab.sh
+++ examples/run_small_microcircuit/run_circuit_bluecellulab.sh
@@ -2,20 +2,14 @@
 
 # circuit simulation
 
-# copy the ../data/tiny_circuits/N_10__top_nodes_dim6__asc/input folder to current directory
-echo "Copying Spike replay input folder ../data/tiny_circuits/N_10__top_nodes_dim6__asc/input to current directory"
-cp -r ../data/tiny_circuits/N_10__top_nodes_dim6__asc/input .
-
 # Remove old compiled mod files
 rm -r arm64/
 # Compile mod files
-# nrnivmodl N_10__top_nodes_dim6__asc/mod
-
 # flag DISABLE_REPORTINGLIB to skip SonataReportHelper.mod and SonataReport.mod from compilation.
-nrnivmodl -incflags "-DDISABLE_REPORTINGLIB" ../data/tiny_circuits/N_10__top_nodes_dim6__asc/mod
+nrnivmodl -incflags "-DDISABLE_REPORTINGLIB" <PATH_TO_MOD_FILES>  # Replace with the actual path to your mod files
 
 echo "Running circuit simulation"
-simulation_config="../data/tiny_circuits/N_10__top_nodes_dim6__asc/simulation_config.json"
+simulation_config="<PATH_TO_SIMULATION_CONFIG_FILE>"  # Replace with the actual path to your simulation config file
 
 num_cores=4
 mpiexec -n $num_cores python run_circuit_bluecellab.py --simulation_config $simulation_config --save-nwb
