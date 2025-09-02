32866748fee3d3a68dda6648190fe0beb794b8a5
Christoph Pokorny <christoph.pokorny@openbraininstitute.org>
2025-09-02 10:01:34 +0200

    Finished linting in morphology_containerization.py

--- obi_one/scientific/morphology_containerization/morphology_containerization.py
+++ obi_one/scientific/morphology_containerization/morphology_containerization.py
@@ -7,12 +7,14 @@ from importlib.metadata import version
 from pathlib import Path
 from typing import ClassVar
 
+import bluepysnap as snap
 import entitysdk.client
 import h5py
 import numpy as np
 import tqdm
 from bluepysnap import BluepySnapError, Circuit
 from morph_tool import convert
+from morphio import MorphioError
 
 from obi_one.core.block import Block
 from obi_one.core.form import Form
@@ -55,12 +57,38 @@ class MorphologyContainerization(MorphologyContainerizationsForm, SingleCoordina
     NO_MORPH_NAME: ClassVar[str] = "_NONE"
 
     @staticmethod
-    def _filter_ext(file_list, ext):
+    def _filter_ext(file_list: list, ext: str) -> list:
         """Filter file list based on file extension."""
-        return list(filter(lambda f: os.path.splitext(f)[1].lower() == f".{ext}", file_list))
+        return list(filter(lambda f: Path(f).suffix.lower() == f".{ext}", file_list))
+
+    @classmethod
+    def _load_node_population(
+        cls, c: Circuit, npop: str
+    ) -> (snap.nodes.NodePopulation, np.ndarray):
+        nodes = c.nodes[npop]
+        if nodes.type != "biophysical":
+            morph_names = None
+        else:
+            morph_names = np.unique(nodes.get(properties="morphology"))
+            if cls.NO_MORPH_NAME in morph_names:
+                L.info(
+                    f"WARNING: Biophysical population '{npop}' has neurons without \
+                        morphologies!"
+                )
+                morph_names = morph_names[morph_names != cls.NO_MORPH_NAME]
+                if len(morph_names) == 0:
+                    msg = f"ERROR: Biophysical population '{npop}' does not have any morphologies!"
+                    raise ValueError(msg)
+            L.info(
+                f"> {len(morph_names)} unique morphologies in population '{npop}' \
+                    ({nodes.size})"
+            )
+        return nodes, morph_names
 
     @staticmethod
-    def _check_morph_folders(nodes, morph_folders_to_delete):
+    def _check_morph_folders(
+        nodes: snap.nodes.NodePopulation, morph_folders_to_delete: list
+    ) -> list:
         """Check existence and contents of morphology folders."""
         morph_folders = {}
         for _morph_ext in ["h5", "asc", "swc"]:
@@ -76,7 +104,10 @@ class MorphologyContainerization(MorphologyContainerizationsForm, SingleCoordina
 
             if (
                 morph_folder is not None
-                and len(self._filter_ext(Path(morph_folder).iterdir(), _morph_ext)) == 0
+                and len(
+                    MorphologyContainerization._filter_ext(Path(morph_folder).iterdir(), _morph_ext)
+                )
+                == 0
             ):
                 # Morphology folder does not contain morphologies
                 morph_folder = None
@@ -89,7 +120,7 @@ class MorphologyContainerization(MorphologyContainerizationsForm, SingleCoordina
         return morph_folders
 
     @staticmethod
-    def _convert_to_h5(morph_folders, morph_names):
+    def _convert_to_h5(morph_folders: dict, morph_names: list) -> Path:
         """If .h5 morphologies not existing, run .asc/.swc to .h5 conversion."""
         h5_folder = morph_folders["h5"]
         if h5_folder is None:
@@ -110,7 +141,7 @@ class MorphologyContainerization(MorphologyContainerizationsForm, SingleCoordina
                     convert(src_file, dest_file)
         return h5_folder
 
-    def _merge_into_h5_container(self, h5_folder, morph_names):
+    def _merge_into_h5_container(self, h5_folder: Path, morph_names: list) -> Path:
         """Merge .h5 morphologies into .h5 container."""
         if h5_folder is None:
             msg = "ERROR: .h5 container folder undefined!"
@@ -130,8 +161,8 @@ class MorphologyContainerization(MorphologyContainerizationsForm, SingleCoordina
         )
         return h5_container
 
-    @staticmethod
-    def _set_global_morph_entry(global_morph_entry, cfg_dict):
+    @classmethod
+    def _set_global_morph_entry(cls, *, global_morph_entry: bool, cfg_dict: dict) -> bool:
         """Check and set path if there is a global entry for morphologies (initially not set)."""
         if global_morph_entry is not None:
             return global_morph_entry
@@ -168,12 +199,14 @@ class MorphologyContainerization(MorphologyContainerizationsForm, SingleCoordina
                     global_morph_entry = True
             if global_morph_entry:
                 # Set .h5 container path globally
-                h5_file = Path(base_path) / self.CONTAINER_FILENAME
+                h5_file = Path(base_path) / cls.CONTAINER_FILENAME
                 cfg_dict["components"]["alternate_morphologies"] = {"h5v1": h5_file}
         return global_morph_entry
 
-    @staticmethod
-    def _set_morph_entries_per_population(global_morph_entry, cfg_dict, nodes):
+    @classmethod
+    def _set_morph_entries_per_population(
+        cls, *, global_morph_entry: bool, cfg_dict: dict, nodes: snap.nodes.NodePopulation
+    ) -> None:
         """Set morphology entries individually per population."""
         if global_morph_entry is None:
             msg = "ERROR: Global morphology entry flag undefined!"
@@ -202,12 +235,12 @@ class MorphologyContainerization(MorphologyContainerizationsForm, SingleCoordina
                 if base_path is None:
                     msg = f"ERROR: Morphology path for population '{nodes.name}' unknown!"
                     raise ValueError(msg)
-                h5_file = Path(base_path) / self.CONTAINER_FILENAME
+                h5_file = Path(base_path) / cls.CONTAINER_FILENAME
                 pop["alternate_morphologies"] = {"h5v1": h5_file}
                 break
 
     @staticmethod
-    def _check_morphologies(circuit_config):
+    def _check_morphologies(circuit_config: Path) -> bool:
         """Check modified circuit by loading some .h5 morphologies from each node population."""
         c = Circuit(circuit_config)
         for npop in c.nodes.population_names:
@@ -219,15 +252,15 @@ class MorphologyContainerization(MorphologyContainerizationsForm, SingleCoordina
                 ].index
                 for nid in node_ids[[0, -1]]:  # First/last node ID (with actual morphology!!)
                     try:
-                        morph = nodes.morph.get(
+                        _ = nodes.morph.get(
                             nid, transform=True, extension="h5"
                         )  # Will throw an error if not accessible
-                    except:
+                    except (MorphioError, ValueError):
                         return False  # Error
         return True  # All successful
 
     @staticmethod
-    def _find_hoc_proc(proc_name, hoc_code):
+    def _find_hoc_proc(proc_name: str, hoc_code: str) -> (int, int, str):
         """Find a procedure with a given name in hoc code."""
         start_idx = hoc_code.find(f"proc {proc_name}")
         if start_idx < 0:
@@ -247,7 +280,7 @@ class MorphologyContainerization(MorphologyContainerizationsForm, SingleCoordina
         return start_idx, end_idx, hoc_code[start_idx : end_idx + 1]
 
     @staticmethod
-    def _find_hoc_header(hoc_code):
+    def _find_hoc_header(hoc_code: str) -> (int, int, str):
         """Find the header section in hoc code."""
         start_idx = hoc_code.find("/*")  # First occurrence
         if start_idx != 0:
@@ -259,22 +292,20 @@ class MorphologyContainerization(MorphologyContainerizationsForm, SingleCoordina
             raise ValueError(msg)
         return start_idx, end_idx, hoc_code[start_idx : end_idx + 2]
 
-    def _update_hoc_files(self, hoc_folder):
+    def _update_hoc_files(self, hoc_folder: str) -> None:
         """Update hoc files in a folder from code of an old to code from a new template."""
         # TODO: CHECK IF .HOC FILE IS ALREADY NEW VERSION??
         # Extract code to be replaced from hoc templates
-        with open(self.initialize.hoc_template_old) as f:
-            tmpl_old = f.read()
-        with open(self.initialize.hoc_template_new) as f:
-            tmpl_new = f.read()
+        tmpl_old = Path(self.initialize.hoc_template_old).read_text(encoding="utf-8")
+        tmpl_new = Path(self.initialize.hoc_template_new).read_text(encoding="utf-8")
 
         proc_name = "load_morphology"
         _, _, hoc_code_old = self._find_hoc_proc(proc_name, tmpl_old)
         _, _, hoc_code_new = self._find_hoc_proc(proc_name, tmpl_new)
 
         # Replace code in hoc files
-        for _file in tqdm.tqdm(os.listdir(hoc_folder), desc="Updating .hoc files"):
-            if os.path.splitext(_file)[1].lower() != ".hoc":
+        for _file in tqdm.tqdm(Path(hoc_folder).iterdir(), desc="Updating .hoc files"):
+            if Path(_file).suffix.lower() != ".hoc":
                 continue
             hoc_file = Path(hoc_folder) / _file
             hoc = Path(hoc_file).read_text(encoding="utf-8")
@@ -293,6 +324,17 @@ class MorphologyContainerization(MorphologyContainerizationsForm, SingleCoordina
             )
             hoc_new = hoc_new.replace(header, header_new)
             Path(hoc_file).write_text(hoc_new, encoding="utf-8")
+
+    def _update_hoc_folder(
+        self, nodes: snap.nodes.NodePopulation, hoc_folders_updated: list
+    ) -> str:
+        hoc_folder = nodes.config["biophysical_neuron_models_dir"]
+        if not Path(hoc_folder).exists():
+            L.info("WARNING: Biophysical neuron models dir missing!")
+        elif hoc_folder not in hoc_folders_updated:
+            self._update_hoc_files(hoc_folder)
+            hoc_folders_updated.append(hoc_folder)
+        return hoc_folder
 
     def run(self, db_client: entitysdk.client.Client = None) -> None:  # noqa: ARG002
         L.info(f"Running morphology containerization for '{self.initialize.circuit_path}'")
@@ -328,23 +370,10 @@ class MorphologyContainerization(MorphologyContainerizationsForm, SingleCoordina
         global_morph_entry = None
 
         for npop in node_populations:
-            nodes = c.nodes[npop]
-            if nodes.type != "biophysical":
+            # Load node population & morphologies
+            nodes, morph_names = self._load_node_population(c, npop)
+            if morph_names is None:
                 continue
-            morph_names = np.unique(nodes.get(properties="morphology"))
-            if self.NO_MORPH_NAME in morph_names:
-                L.info(
-                    f"WARNING: Biophysical population '{npop}' has neurons without \
-                        morphologies!"
-                )
-                morph_names = morph_names[morph_names != self.NO_MORPH_NAME]
-                if len(morph_names) == 0:
-                    msg = f"ERROR: Biophysical population '{npop}' does not have any morphologies!"
-                    raise ValueError(msg)
-            L.info(
-                f"> {len(morph_names)} unique morphologies in population '{npop}' \
-                    ({nodes.size})"
-            )
 
             # Check morphology folders
             morph_folders = self._check_morph_folders(nodes, morph_folders_to_delete)
@@ -369,21 +398,20 @@ class MorphologyContainerization(MorphologyContainerizationsForm, SingleCoordina
                 cfg_dict = json.load(f)
 
             # Check and set if there is a global entry for morphologies (initially not set)
-            global_morph_entry = self._set_global_morph_entry(global_morph_entry, cfg_dict)
+            global_morph_entry = self._set_global_morph_entry(
+                global_morph_entry=global_morph_entry, cfg_dict=cfg_dict
+            )
 
             # Otherwise, set individually per population
-            self._set_morph_entries_per_population(global_morph_entry, cfg_dict, nodes)
+            self._set_morph_entries_per_population(
+                global_morph_entry=global_morph_entry, cfg_dict=cfg_dict, nodes=nodes
+            )
 
             with Path(circuit_config).open("w", encoding="utf-8") as f:
                 json.dump(cfg_dict, f, indent=2)
 
             # Update hoc files (in place)
-            hoc_folder = nodes.config["biophysical_neuron_models_dir"]
-            if not Path(hoc_folder).exists():
-                L.info("WARNING: Biophysical neuron models dir missing!")
-            elif hoc_folder not in hoc_folders_updated:
-                self._update_hoc_files(hoc_folder)
-                hoc_folders_updated.append(hoc_folder)
+            self._update_hoc_folder(nodes, hoc_folders_updated)
 
         # Clean up morphology folders with individual morphologies
         L.info(f"Cleaning morphology folders: {morph_folders_to_delete}")
