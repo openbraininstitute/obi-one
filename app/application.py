0ca1c531adb16dcdfea026451566bc274b0b33b9
Daniel Keller <dkeller@daniels-macbook-pro.home>
2025-09-09 02:33:12 +0200

    migrating some endpoint functionality to frontend

--- app/application.py
+++ app/application.py
@@ -17,9 +17,6 @@ from app.config import settings
 from app.dependencies.auth import user_verified
 from app.endpoints.declared_endpoints import activate_declared_endpoints
 from app.endpoints.generated_endpoints import activate_generated_endpoints
-from app.endpoints.licenses import router as licenses_router
-from app.endpoints.mtypes import router as mtypes_router
-from app.endpoints.subject_data import router as subject_data_router
 from app.errors import ApiError, ApiErrorCode
 from app.logger import L
 from app.schemas.base import ErrorResponse
