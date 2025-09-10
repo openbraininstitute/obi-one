641c768cfa56028226875820474359bb99b05661
James Isbister <isbisterjb@gmail.com>
2025-09-10 08:15:09 +0200

    working changes

---
+++ tests/data/simulation_serialization.py
@@ -0,0 +1,44 @@
+{
+  "obi_one_version": "2025.8.3",
+  "type": "Simulation",
+  "idx": 0,
+  "coordinate_output_root": "/tmp/tmp4mnmejp3/0",
+  "scan_output_root": "/tmp/tmp4mnmejp3",
+  "single_coordinate_scan_params": {
+    "scan_params": [],
+    "nested_coordinate_subpath_str": "Simulation",
+    "type": "SingleCoordinateScanParams"
+  },
+  "timestamps": {},
+  "stimuli": {},
+  "recordings": {},
+  "neuron_sets": {
+    "NeuronSet_0": {
+      "type": "AllNeurons",
+      "sample_percentage": 100,
+      "sample_seed": 1
+    }
+  },
+  "synaptic_manipulations": {},
+  "initialize": {
+    "type": "SimulationsForm.Initialize",
+    "circuit": {
+      "id_str": "2288cb29-c16f-4e9e-b110-ce62f55de41e",
+      "type": "CircuitFromID"
+    },
+    "node_set": {
+      "block_dict_name": "neuron_sets",
+      "block_name": "NeuronSet_0",
+      "type": "NeuronSetReference"
+    },
+    "simulation_length": 1000,
+    "extracellular_calcium_concentration": 1.1,
+    "v_init": -80,
+    "random_seed": 1
+  },
+  "info": {
+    "type": "Info",
+    "campaign_name": "k",
+    "campaign_description": "n"
+  }
+}
