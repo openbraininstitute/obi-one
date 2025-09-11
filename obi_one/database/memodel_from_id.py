f577ac97c007f5d7e959324b5971bd5f898b9a7c
ilkankilic <ilkan.kilic@openbraininstitute.org>
2025-09-11 16:56:05 +0200

    add memodel_from_id function

---
+++ obi_one/database/memodel_from_id.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from typing import ClassVar
+
+from entitysdk.client import Client
+from entitysdk.models import MEModel
+from entitysdk.models.entity import Entity
+from entitysdk.staging.memodel import stage_sonata_from_memodel
+from pydantic import PrivateAttr
+
+from obi_one.database.entity_from_id import EntityFromID
+from obi_one.scientific.circuit.circuit import Circuit
+
+
+class MEModelFromID(EntityFromID):
+    entitysdk_class: ClassVar[type[Entity]] = MEModel
+    _entity: MEModel | None = PrivateAttr(default=None)
+
+    def stage_as_form_circuit(
+        self, db_client: Client = None, dest_dir: Path | None = None
+    ) -> Circuit:
+        memodel = super().entity(db_client)
+
+        circuit_config_path = stage_sonata_from_memodel(
+            client=db_client, memodel=memodel, output_dir=dest_dir
+        )
+
+        return Circuit(name="single_cell", path=str(circuit_config_path))
