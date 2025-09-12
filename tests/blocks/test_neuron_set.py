27aa11eaac622f835ebba2d97bd086786522f84c
Christoph Pokorny <christoph.pokorny@openbraininstitute.org>
2025-09-12 12:13:47 +0200

    Added writing to node sets file test

--- tests/blocks/test_neuron_set.py
+++ tests/blocks/test_neuron_set.py
@@ -348,3 +348,54 @@ def test_simplex_neuron_sets():
         np.testing.assert_array_equal(sorted(neuron_set_ids), exp)
         assert neuron_set_def["population"] == circuit.default_population_name
         np.testing.assert_array_equal(sorted(neuron_set_def["node_id"]), exp)
+
+
+def test_write_to_node_set_file(tmp_path):
+    # Load circuit
+    circuit_name = "N_10__top_nodes_dim6"
+    circuit = obi.Circuit(
+        name=circuit_name, path=str(CIRCUIT_DIR / circuit_name / "circuit_config.json")
+    )
+    c = circuit.sonata_circuit
+    
+    # Write new file
+    neuron_set = obi.CombinedNeuronSet(node_sets=("Layer1", "Layer2", "Layer3"))
+    nset_file = neuron_set.to_node_set_file(circuit, circuit.default_population_name, output_path=tmp_path, overwrite_if_exists=False, optional_node_set_name="L123")
+    assert Path(nset_file).exists()
+    
+    # Write again w/o overwriting --> Must raise an error
+    with pytest.raises(ValueError, match=f"Output file '{tmp_path / 'node_sets.json'}' already exists! Delete file or choose to append or overwrite."):
+        nset_file = neuron_set.to_node_set_file(circuit, circuit.default_population_name, output_path=tmp_path, overwrite_if_exists=False, optional_node_set_name="L123")
+    
+    # Write again with overwriting --> No error
+    nset_file = neuron_set.to_node_set_file(circuit, circuit.default_population_name, output_path=tmp_path, overwrite_if_exists=True, optional_node_set_name="L123")
+    assert Path(nset_file).exists()
+    
+    # Append to existing file, but name already exists --> Must raise an error
+    with pytest.raises(ValueError, match="Appending not possible, node set 'L123' already exists!"):
+        nset_file = neuron_set.to_node_set_file(circuit, circuit.default_population_name, output_path=tmp_path, append_if_exists=True, optional_node_set_name="L123")
+    
+    # Append to existing file
+    neuron_set = obi.CombinedNeuronSet(node_sets=("Layer4", "Layer5", "Layer6"))
+    nset_file = neuron_set.to_node_set_file(circuit, circuit.default_population_name, output_path=tmp_path, append_if_exists=True, optional_node_set_name="L456")
+    assert Path(nset_file).exists()
+    
+    # Check if new node sets exist in the .json file
+    with Path(nset_file).open(encoding="utf-8") as f:
+        node_sets = json.load(f)
+    
+    assert "L123" in node_sets
+    assert "L456" in node_sets
+    
+    assert node_sets["L123"] == ["Layer1", "Layer2", "Layer3"]
+    assert node_sets["L456"] == ["Layer4", "Layer5", "Layer6"]
+    
+    # Check that original node sets are preserved in new node sets file
+    orig_node_sets = circuit.sonata_circuit.node_sets.content
+    for k, v in orig_node_sets.items():
+        assert k in node_sets
+        assert node_sets[k] == v
+    
+    # Check that original node sets are unchanged
+    assert "L123" not in orig_node_sets
+    assert "L456" not in orig_node_sets
