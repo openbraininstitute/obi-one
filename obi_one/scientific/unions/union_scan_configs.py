b46d22bb7eda869700c94aeeec0f602b37731d6f
James Isbister <isbisterjb@gmail.com>
2025-09-23 21:26:47 +0200

    SimulationsForm added to ScanConfigsUnion

--- obi_one/scientific/unions/union_scan_configs.py
+++ obi_one/scientific/unions/union_scan_configs.py
@@ -4,8 +4,9 @@ from pydantic import Discriminator
 
 from obi_one.scientific.tasks.example_task_1 import ExampleScanConfig
 from obi_one.scientific.tasks.example_task_2 import ExampleScanConfig2
+from obi_one.scientific.tasks.simulations import SimulationsForm
 
 ScanConfigsUnion = Annotated[
-    ExampleScanConfig | ExampleScanConfig2,
+    ExampleScanConfig | ExampleScanConfig2 | SimulationsForm,
     Discriminator("type"),
 ]
