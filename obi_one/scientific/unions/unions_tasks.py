989701debab6a8b5b26a3fc7228b084779a979c2
James Isbister <isbisterjb@gmail.com>
2025-09-23 23:23:10 +0200

    get_configs_task_type fix

--- obi_one/scientific/unions/unions_tasks.py
+++ obi_one/scientific/unions/unions_tasks.py
@@ -15,7 +15,7 @@ inner, *_ = get_args(TasksUnion)
 task_types = get_args(inner)
 
 _task_configs_map = {task.__name__: task.model_fields["config"].annotation for task in task_types}
-_config_tasks_map = {task.model_fields["config"].annotation: task.__name__ for task in task_types}
+_config_tasks_map = {task.model_fields["config"].annotation: task.__class__ for task in task_types}
 
 
 def get_tasks_config_type(task: TasksUnion) -> type:
