72ac0c6553a620ef7ff217ae329d06cd7dc6000a
James Isbister <isbisterjb@gmail.com>
2025-09-23 23:10:04 +0200

    test_deserialization.py updated

--- tests/deserialization/test_deserialization.py
+++ tests/deserialization/test_deserialization.py
@@ -12,6 +12,12 @@ def simulation_json():
     return json.loads((DATA_DIR / "simulation_serialization.json").read_bytes())
 
 
+def run_task_for_single_simulation_generation_config(single_config):
+    task_type = obi.get_task_config_type(single_config)
+    task = task_type(config=single_config)
+    task.execute()
+
+
 def test_deserialization(tmp_path):
     simulation_json_path = DATA_DIR / "simulation_serialization.json"
 
@@ -19,9 +25,9 @@ def test_deserialization(tmp_path):
     simulation = obi.deserialize_obi_object_from_json_data(data)
     assert isinstance(simulation, obi.Simulation)
     simulation.coordinate_output_root = tmp_path / "simulation_output"
-    simulation.generate()
+    run_task_for_single_simulation_generation_config(single_config=simulation)
 
     simulation = obi.deserialize_obi_object_from_json_file(simulation_json_path)
     assert isinstance(simulation, obi.Simulation)
     simulation.coordinate_output_root = tmp_path / "simulation_output_2"
-    simulation.generate()
+    run_task_for_single_simulation_generation_config(single_config=simulation)
