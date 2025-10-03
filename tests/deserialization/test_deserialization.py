4292775caebfa38f5d0055f62c1e22e60d964d66
James Isbister <isbisterjb@gmail.com>
2025-10-03 09:43:21 +0200

    further deserialization tests

--- tests/deserialization/test_deserialization.py
+++ tests/deserialization/test_deserialization.py
@@ -8,10 +8,12 @@ from tests.utils import DATA_DIR
 def test_deserialization(tmp_path):
 
 
+    model_dumps_dir = DATA_DIR / "model_dumps"
+
     """
     Test deserialization of simulation
     """
-    simulation_json_path = DATA_DIR / "circuit_simulation_single_config_serialization.json"
+    simulation_json_path = model_dumps_dir / "circuit_simulation_single_config_serialization.json"
 
     data = json.loads(simulation_json_path.read_bytes())
     simulation = obi.deserialize_obi_object_from_json_data(data)
@@ -25,27 +27,29 @@ def test_deserialization(tmp_path):
     obi.run_task_for_single_config(single_config=simulation)
 
     """
-    Test deserialization of simulation campaign
+    Test deserialization of grid_scan_task
     """
-    simulation_campaign_json_path = DATA_DIR / "grid_scan_serialization.json"
+    grid_scan_task_json_path = model_dumps_dir / "grid_scan_task_serialization.json"
 
-    data = json.loads(simulation_campaign_json_path.read_bytes())
-    simulation_campaign = obi.deserialize_obi_object_from_json_data(data)
-    assert isinstance(simulation_campaign, obi.GridScanGenerationTask)
-    simulation_campaign.output_root = tmp_path / "simulation_output_3"
-    simulation_campaign.execute()
+    data = json.loads(grid_scan_task_json_path.read_bytes())
+    grid_scan_task = obi.deserialize_obi_object_from_json_data(data)
+    assert isinstance(grid_scan_task, obi.GridScanGenerationTask)
+    grid_scan_task.output_root = tmp_path / "simulation_output_3"
+    grid_scan_task.execute()
+    obi.run_tasks_for_generated_scan(grid_scan_task)
 
-    simulation_campaign = obi.deserialize_obi_object_from_json_file(simulation_campaign_json_path)
-    assert isinstance(simulation_campaign, obi.GridScanGenerationTask)
-    simulation_campaign.output_root = tmp_path / "simulation_output_4"
-    simulation_campaign.execute()
+    grid_scan_task = obi.deserialize_obi_object_from_json_file(grid_scan_task_json_path)
+    assert isinstance(grid_scan_task, obi.GridScanGenerationTask)
+    grid_scan_task.output_root = tmp_path / "simulation_output_4"
+    grid_scan_task.execute()
+    obi.run_tasks_for_generated_scan(grid_scan_task)
 
 
 
     """
     Test deserialization of depreceted simulation (based on SimulationsForm)
     """
-    simulation_json_path = DATA_DIR / "deprecated_simulation_serialization.json"
+    simulation_json_path = model_dumps_dir / "deprecated_simulation_serialization.json"
 
     data = json.loads(simulation_json_path.read_bytes())
     simulation = obi.deserialize_obi_object_from_json_data(data)
@@ -56,3 +60,21 @@ def test_deserialization(tmp_path):
     assert isinstance(simulation, obi.Simulation)
     simulation.coordinate_output_root = tmp_path / "simulation_output_6"
     obi.run_task_for_single_config(single_config=simulation)
+
+    """
+    Test deserialization of grid_scan_task
+    """
+    grid_scan_task_json_path = model_dumps_dir / "grid_scan_task_serialization.json"
+
+    data = json.loads(grid_scan_task_json_path.read_bytes())
+    grid_scan_task = obi.deserialize_obi_object_from_json_data(data)
+    assert isinstance(grid_scan_task, obi.GridScanGenerationTask)
+    grid_scan_task.output_root = tmp_path / "simulation_output_7"
+    grid_scan_task.execute()
+    obi.run_tasks_for_generated_scan(grid_scan_task)
+
+    grid_scan_task = obi.deserialize_obi_object_from_json_file(grid_scan_task_json_path)
+    assert isinstance(grid_scan_task, obi.GridScanGenerationTask)
+    grid_scan_task.output_root = tmp_path / "simulation_output_8"
+    grid_scan_task.execute()
+    obi.run_tasks_for_generated_scan(grid_scan_task)
