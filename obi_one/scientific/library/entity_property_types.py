47bcbb5409b3fed7334a342ffbc8ab0b0c9f837f
James Isbister <isbisterjb@gmail.com>
2025-10-06 09:31:17 +0200

    CircuitPropertyType strings updated

--- obi_one/scientific/library/entity_property_types.py
+++ obi_one/scientific/library/entity_property_types.py
@@ -2,7 +2,7 @@ from enum import StrEnum
 
 
 class CircuitPropertyType(StrEnum):
-    NODE_SET = "CircuitNodeSet"
-    POPULATION = "CircuitPopulation"
-    BIOPHYSICAL_POPULATION = "CircuitBiophysicalPopulation"
-    VIRTUAL_POPULATION = "CircuitVirtualPopulation"
+    NODE_SET = "Circuit.NodeSet"
+    POPULATION = "Circuit.Population"
+    BIOPHYSICAL_POPULATION = "Circuit.BiophysicalPopulation"
+    VIRTUAL_POPULATION = "Circuit.VirtualPopulation"
