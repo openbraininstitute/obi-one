d1c7616395d5ee8101b58dac585614eac8ea5b2a
Daniel Keller <dkeller@mac.home>
2025-10-09 15:21:45 +0200

    added endpoint for testing contributed nwb files

--- app/endpoints/declared_endpoints.py
+++ app/endpoints/declared_endpoints.py
@@ -2,6 +2,7 @@ import asyncio
 import pathlib
 import tempfile
 import zipfile
+import subprocess
 from http import HTTPStatus
 from typing import Annotated, Literal
 
@@ -251,6 +252,87 @@ def activate_test_endpoint(router: APIRouter) -> None:
                     L.error(f"Error deleting temporary files: {e!s}")
 
 
+async def _process_nwb(
+    file: UploadFile, temp_file_path: str, file_extension: str
+)  -> None:
+    """Validate nwb file with pynwb."""
+    try:
+        command = ["pynwb-validate", temp_file_path]
+        # Run the command
+        result = subprocess.run(
+        command,
+        check=True,  # Raise an exception for non-zero return codes (i.e., errors)
+        capture_output=True, # Capture stdout and stderr
+        text=True # Decode stdout and stderr as text
+        )
+    except Exception as e:
+        L.error(f"Nwb error validating file {file.filename}: {e!s}")
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail={
+                "code": ApiErrorCode.BAD_REQUEST,
+                "detail": f"Failed to validate the file: {e!s}",
+            },
+        ) from e
+    else:
+        return 
+
+
+async def _validate_and_read_nwb_file(file: UploadFile) -> tuple[bytes, str]:
+    """Validates file extension and reads content."""
+    L.info(f"Received file upload: {file.filename}")
+    allowed_extensions = {".nwb"}
+    file_extension = f".{file.filename.split('.')[-1].lower()}" if file.filename else ""
+
+    if not file.filename or file_extension not in allowed_extensions:
+        L.error(f"Invalid file extension: {file_extension}")
+        valid_extensions = ", ".join(allowed_extensions)
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail={
+                "code": ApiErrorCode.BAD_REQUEST,
+                "detail": f"Invalid file extension. Must be one of {valid_extensions}",
+            },
+        )
+
+    content = await file.read()
+    if not content:
+        _handle_empty_file(file)
+
+    return content, file_extension
+
+
+def activate_test_nwb_endpoint(router: APIRouter) -> None:
+    """Define neuron file test endpoint."""
+
+    @router.post(
+        "/test-nwb-file",
+        summary="Validate new format.",
+        description="Tests a new file (.nwb) with basic validation.",
+    )
+    async def test_nwb_file(
+        file: Annotated[UploadFile, File(description="Nwb file to upload (.swc, .h5, or .asc)")],
+    ) -> FileResponse:
+        content, file_extension = await _validate_and_read_nwb_file(file)
+
+        temp_file_path = ""
+        
+        try:
+            with tempfile.NamedTemporaryFile(delete=False, suffix=file_extension) as temp_file:
+                temp_file.write(content)
+                temp_file_path = temp_file.name
+            await _process_nwb(
+                file=file, temp_file_path=temp_file_path, file_extension=file_extension
+            )
+            return 
+        finally:
+            if temp_file_path:
+                try:
+                    pathlib.Path(temp_file_path).unlink(missing_ok=True)
+                except OSError as e:
+                    L.error(f"Error deleting temporary files: {e!s}")
+
+
 def activate_circuit_endpoints(router: APIRouter) -> None:
     """Define circuit-related endpoints."""
 
@@ -350,5 +432,6 @@ def activate_declared_endpoints(router: APIRouter) -> APIRouter:
     activate_morphology_endpoint(router)
     activate_ephys_endpoint(router)
     activate_test_endpoint(router)
+    activate_test_nwb_endpoint(router)
     activate_circuit_endpoints(router)
     return router
