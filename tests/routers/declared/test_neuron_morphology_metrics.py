c61b88c813ed089465118cfb1f1bde9c0dca2c6b
Christoph Pokorny <christoph.pokorny@openbraininstitute.org>
2025-10-09 16:19:24 +0200

    Fixed error code in test

--- tests/routers/declared/test_neuron_morphology_metrics.py
+++ tests/routers/declared/test_neuron_morphology_metrics.py
@@ -66,8 +66,8 @@ def test_get_not_found(client, morphology_json, monkeypatch):
     response = client.get(f"{ROUTE}/{entity_id}")
     assert response.status_code == 500
     assert response.json() == {
-        "message": "Asset not found",
-        "error_code": "NOT_FOUND",
+        "message": "Internal error retrieving the asset.",
+        "error_code": "INTERNAL_ERROR",
         "details": None,
     }
     assert entitysdk_client_mock.get_entity.call_count == 1
