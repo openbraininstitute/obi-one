b5be23102752de2793164a1e938bab824d5c60ec
James Isbister <isbisterjb@gmail.com>
2025-10-14 20:44:13 +0200

    File renames

--- obi_one/scientific/blocks/neuron_sets/base.py
+++ obi_one/scientific/blocks/neuron_sets/base.py
@@ -40,59 +40,6 @@ with contextlib.suppress(ImportError):  # Try to import connalysis
     from obi_one.scientific.library.simplex_extractors import simplex_submat
 
 
-class NeuronPropertyFilter(OBIBaseModel, abc.ABC):
-    filter_dict: dict[str, list] = Field(
-        name="Filter",
-        description="Filter dictionary. Note as this is NOT a Block and the list here is \
-                    not to support multi-dimensional parameters but to support a key-value pair \
-                    with multiple values i.e. {'layer': ['2', '3']}}",
-        default={},
-    )
-
-    @model_validator(mode="after")
-    def check_filter_dict_values(self) -> Self:
-        for key, values in self.filter_dict.items():
-            if not isinstance(values, list) or len(values) == 0:
-                msg = f"Filter key '{key}' must have a non-empty list of values."
-                raise ValueError(msg)
-        return self
-
-    @property
-    def filter_keys(self) -> list[str]:
-        return list(self.filter_dict.keys())
-
-    @property
-    def filter_values(self) -> list[list]:
-        return list(self.filter_dict.values())
-
-    def filter(self, df_in: pd.DataFrame, *, reindex: bool = True) -> pd.DataFrame:
-        ret = df_in
-        for filter_key, _filter_value in self.filter_dict.items():
-            filter_value = [str(_entry) for _entry in _filter_value]
-            vld = ret[filter_key].astype(str).isin(filter_value)
-            ret = ret.loc[vld]
-            if reindex:
-                ret = ret.reset_index(drop=True)
-        return ret
-
-    def test_validity(self, circuit: Circuit, node_population: str) -> None:
-        circuit_prop_names = circuit.sonata_circuit.nodes[node_population].property_names
-
-        if not all(_prop in circuit_prop_names for _prop in self.filter_keys):
-            msg = f"Invalid neuron properties! Available properties: {circuit_prop_names}"
-            raise ValueError(msg)
-
-    def __repr__(self) -> str:
-        """Return a string representation of the NeuronPropertyFilter object."""
-        if len(self.filter_dict) == 0:
-            return "NoFilter"
-        string_rep = ""
-        for filter_key, filter_value in self.filter_dict.items():
-            string_rep += f"{filter_key}="
-            for value in filter_value:
-                string_rep += f"{value},"
-        return string_rep[:-1]  # Remove trailing comma and space
-
 
 class AbstractNeuronSet(Block, abc.ABC):
     """Base class representing a neuron set which can be turned into a SONATA node set by either
