46d8c9a4ea48ba093c4e8af9f8b41564e434fad5
James Isbister <isbisterjb@gmail.com>
2025-10-20 15:42:44 +0200

    entity_type in separate variable

--- obi_one/core/launch_task_for_single_config_asset.py
+++ obi_one/core/launch_task_for_single_config_asset.py
@@ -9,26 +9,28 @@ L = logging.getLogger(__name__)
 
 N_ARGS = 4
 
-# python launch_task_for_single_config_asset.py "Simulation" "1569a81e-b578-4c39-a3a9-f9a05f123db9" "c9edaedf-e5c0-4643-979c-47375f3160e0"
+# python launch_task_for_single_config_asset.py Simulation 1569a81e-b578-4c39-a3a9-f9a05f123db9 c9edaedf-e5c0-4643-979c-47375f3160e0
 
 def main() -> None:
     if len(sys.argv) == N_ARGS:
-        entity_type, entity_id, config_asset_id = sys.argv[1], sys.argv[2], sys.argv[3]
+        entity_type_str, entity_id, config_asset_id = sys.argv[1], sys.argv[2], sys.argv[3]
 
         L.info(
             f"Running task for \
-                entity type: {entity_type}, \
+                entity type: {entity_type_str}, \
                 entity ID: {entity_id}, \
                 asset ID: {config_asset_id}"
         )
 
-        db_client = [HOW DO WE INITIALIZE CLIENT? PASSING TOKEN, LAB ID, PROJECT ID AS PARAMETERS?]
+        entity_type = getattr(entitysdk.models, entity_type_str)
 
+        # db_client = [HOW DO WE INITIALIZE CLIENT? PASSING TOKEN, LAB ID, PROJECT ID AS PARAMETERS?]
+        
         run_task_for_single_config_asset(
-            entity_type=getattr(entitysdk.models, entity_type),
+            entity_type=entity_type,
             entity_id=entity_id,
             config_asset_id=config_asset_id,
-            db_client=db_client
+            # db_client=db_client
         )
 
     else:
