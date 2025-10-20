42a01f5411ffe44811ee62ca0fbb9a989044787a
James Isbister <isbisterjb@gmail.com>
2025-10-20 15:48:04 +0200

    use token, lab_id, project_id to init client

--- obi_one/core/launch_task_for_single_config_asset.py
+++ obi_one/core/launch_task_for_single_config_asset.py
@@ -2,18 +2,24 @@ import logging
 import sys
 
 import entitysdk
+from entitysdk import Client, ProjectContext
 
 from obi_one.core.run_tasks import run_task_for_single_config_asset
 
 L = logging.getLogger(__name__)
 
-N_ARGS = 4
+N_ARGS = 7
 
-# python launch_task_for_single_config_asset.py Simulation 1569a81e-b578-4c39-a3a9-f9a05f123db9 c9edaedf-e5c0-4643-979c-47375f3160e0
+# python launch_task_for_single_config_asset.py Simulation 1569a81e-b578-4c39-a3a9-f9a05f123db9 c9edaedf-e5c0-4643-979c-47375f3160e0 TOKEN LAB_ID PROJECT_ID
 
 def main() -> None:
     if len(sys.argv) == N_ARGS:
-        entity_type_str, entity_id, config_asset_id = sys.argv[1], sys.argv[2], sys.argv[3]
+        entity_type_str= sys.argv[1]
+        entity_id = sys.argv[2]
+        config_asset_id = sys.argv[3]
+        token = sys.argv[4]
+        lab_id = sys.argv[5]
+        project_id = sys.argv[6]
 
         L.info(
             f"Running task for \
@@ -24,13 +30,14 @@ def main() -> None:
 
         entity_type = getattr(entitysdk.models, entity_type_str)
 
-        # db_client = [HOW DO WE INITIALIZE CLIENT? PASSING TOKEN, LAB ID, PROJECT ID AS PARAMETERS?]
+        project_context = ProjectContext(virtual_lab_id=lab_id, project_id=project_id)
+        db_client = Client(token_manager=token, project_context=project_context)
         
         run_task_for_single_config_asset(
             entity_type=entity_type,
             entity_id=entity_id,
             config_asset_id=config_asset_id,
-            # db_client=db_client
+            db_client=db_client
         )
 
     else:
