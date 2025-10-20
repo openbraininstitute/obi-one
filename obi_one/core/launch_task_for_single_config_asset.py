57b577447d312b626346a2eb5195adbc75fa1a76
James Isbister <isbisterjb@gmail.com>
2025-10-20 15:48:43 +0200

    Linting

--- obi_one/core/launch_task_for_single_config_asset.py
+++ obi_one/core/launch_task_for_single_config_asset.py
@@ -10,11 +10,13 @@ L = logging.getLogger(__name__)
 
 N_ARGS = 7
 
-# python launch_task_for_single_config_asset.py Simulation 1569a81e-b578-4c39-a3a9-f9a05f123db9 c9edaedf-e5c0-4643-979c-47375f3160e0 TOKEN LAB_ID PROJECT_ID
+# python launch_task_for_single_config_asset.py Simulation
+#  1569a81e-b578-4c39-a3a9-f9a05f123db9 c9edaedf-e5c0-4643-979c-47375f3160e0 TOKEN LAB_ID PROJECT_ID
+
 
 def main() -> None:
     if len(sys.argv) == N_ARGS:
-        entity_type_str= sys.argv[1]
+        entity_type_str = sys.argv[1]
         entity_id = sys.argv[2]
         config_asset_id = sys.argv[3]
         token = sys.argv[4]
@@ -32,16 +34,17 @@ def main() -> None:
 
         project_context = ProjectContext(virtual_lab_id=lab_id, project_id=project_id)
         db_client = Client(token_manager=token, project_context=project_context)
-        
+
         run_task_for_single_config_asset(
             entity_type=entity_type,
             entity_id=entity_id,
             config_asset_id=config_asset_id,
-            db_client=db_client
+            db_client=db_client,
         )
 
     else:
         L.info(f"Please provide {N_ARGS} arguments only.")
+
 
 if __name__ == "__main__":
     main()
