f913d89a8943b68481507279ebad36a1a07e187d
ilkankilic <ilkan.kilic@openbraininstitute.org>
2025-10-24 10:34:56 +0200

    fix confirm_single_neuron

--- obi_one/scientific/library/memodel_circuit.py
+++ obi_one/scientific/library/memodel_circuit.py
@@ -23,7 +23,14 @@ class MEModelWithSynapsesCircuit(Circuit):
     @model_validator(mode="after")
     def confirm_single_neuron(self) -> Self:
         sonata_circuit = self.sonata_circuit
-        if len(sonata_circuit.nodes.ids()) != 1:
+        total_real = 0
+        for pop_name in sonata_circuit.nodes.population_names:
+            pop = sonata_circuit.nodes[pop_name]
+            if pop.type != "virtual":
+                n = pop.size
+                total_real += n
+
+        if total_real != 1:
             msg = "MEModelWithSynapsesCircuit must contain exactly one neuron."
             raise OBIONEError(msg)
         return self
