0400030d2d0e8e4bdbe7a6cd2c5dedfda178750d
James Isbister <isbisterjb@gmail.com>
2025-10-27 16:22:11 +0100

    entity_core_api_url cli argument

--- obi_one/core/launch_task_for_single_config_asset.py
+++ obi_one/core/launch_task_for_single_config_asset.py
@@ -21,43 +21,51 @@ def main() -> int:
         --entity_id babb299c-782a-41f1-b782-bc4c5da45462
         --config_asset_id 12eb6209-a4a1-40ad-ae2e-4b5c137e42a8
         --scan_output_root ./grid_scan
+        --entity_cache True
+        --entity_core_api_url https://staging.openbraininstitute.org/api/entitycore
         --lab_id e6030ed8-a589-4be2-80a6-f975406eb1f6
         --project_id 2720f785-a3a2-4472-969d-19a53891c817
 
     Environment Variables Required:
         OBI_AUTHENTICATION_TOKEN: Your authentication token for the platform.
     """
-    # try:
-    parser = argparse.ArgumentParser(
-        description="Script to launch a task for a single configuration asset."
-    )
+    try:
+        parser = argparse.ArgumentParser(
+            description="Script to launch a task for a single configuration asset."
+        )
 
-    parser.add_argument("--entity_type", required=True, help="EntitySDK Entity type as string")
-    parser.add_argument("--entity_id", required=True, help="Entity ID as string")
-    parser.add_argument("--config_asset_id", required=True, help="Configuration Asset ID as string")
-    parser.add_argument(
-        "--scan_output_root",
-        required=True,
-        help="scan_output_root as string. The coordinate output root will be relative to this in\
-                a directory named using the idx of the single coordinate config.",
-    )
-    parser.add_argument("--entity_cache", required=True, help="Boolean flag for campaign entity caching.\
-                                                    Check if enabled for particular EntityFromID types.")
-    parser.add_argument("--lab_id", required=True, help="Virtual Lab ID as string")
-    parser.add_argument("--project_id", required=True, help="Project ID as string.")
+        parser.add_argument("--entity_type", required=True, help="EntitySDK Entity type as string")
+        parser.add_argument("--entity_id", required=True, help="Entity ID as string")
+        parser.add_argument(
+            "--config_asset_id", required=True, help="Configuration Asset ID as string"
+        )
+        parser.add_argument(
+            "--scan_output_root",
+            required=True,
+            help="scan_output_root as string. The coordinate output root will be relative to this\
+                in a directory named using the idx of the single coordinate config.",
+        )
+        parser.add_argument(
+            "--entity_cache",
+            required=True,
+            help="Boolean flag for campaign entity caching.\
+                    Check if enabled for particular EntityFromID types.",
+        )
+        parser.add_argument("--lab_id", required=True, help="Virtual Lab ID as string")
+        parser.add_argument("--project_id", required=True, help="Project ID as string.")
 
-    args = parser.parse_args()
+        args = parser.parse_args()
 
-    # except ValueError as e:
-    #     L.error(f"Argument parsing error: {e}")
-    #     return 1
+    except ValueError as e:
+        L.error(f"Argument parsing error: {e}")
+        return 1
 
-    # try:
     entity_type_str = args.entity_type
     entity_id = args.entity_id
     config_asset_id = args.config_asset_id
     scan_output_root = args.scan_output_root
     entity_cache = args.entity_cache
+    entity_core_api_url = args.entity_core_api_url
     lab_id = args.lab_id
     project_id = args.project_id
 
@@ -67,7 +75,7 @@ def main() -> int:
 
     project_context = ProjectContext(virtual_lab_id=lab_id, project_id=project_id)
     db_client = Client(
-        api_url="https://staging.openbraininstitute.org/api/entitycore",
+        api_url=entity_core_api_url,
         token_manager=token,
         project_context=project_context,
     )
@@ -81,18 +89,7 @@ def main() -> int:
         entity_cache=entity_cache,
     )
 
-    # except OBIONEError as e:
-    #     L.error(f"Error occurred: {e}")
-    #     return 1
-    # except ValueError as e:
-    #     L.error(f"Value error: {e}")
-    #     return 1
-    # except Exception as e:
-    #     L.error(f"Unexpected error occurred: {e}")
-    #     return 1
-    # else:
-    #     L.info("Task launched successfully.")
-    #     return 0
+    return 0
 
 
 if __name__ == "__main__":
