e78f45f8062a1f6a561dd93b535a7ca5894d7226
James Isbister <isbisterjb@gmail.com>
2025-10-29 11:29:32 +0100

    Add obi-one exception

--- obi_one/core/launch_task_for_single_config_asset.py
+++ obi_one/core/launch_task_for_single_config_asset.py
@@ -6,6 +6,7 @@ import sys
 import entitysdk
 from entitysdk import Client, ProjectContext
 
+from obi_one.core.exception import OBIONEError
 from obi_one.core.run_tasks import run_task_for_single_config_asset
 
 L = logging.getLogger(__name__)
@@ -16,14 +17,14 @@ def main() -> int:
 
     Example usage.
 
-    python launch_task_for_single_config_asset.py 
-        --entity_type Simulation 
-        --entity_id babb299c-782a-41f1-b782-bc4c5da45462 
-        --config_asset_id 12eb6209-a4a1-40ad-ae2e-4b5c137e42a8 
-        --scan_output_root ./grid_scan 
-        --entity_cache True 
-        --entity_core_api_url https://staging.openbraininstitute.org/api/entitycore 
-        --lab_id e6030ed8-a589-4be2-80a6-f975406eb1f6 
+    python launch_task_for_single_config_asset.py
+        --entity_type Simulation
+        --entity_id babb299c-782a-41f1-b782-bc4c5da45462
+        --config_asset_id 12eb6209-a4a1-40ad-ae2e-4b5c137e42a8
+        --scan_output_root ./grid_scan
+        --entity_cache True
+        --entity_core_api_url https://staging.openbraininstitute.org/api/entitycore
+        --lab_id e6030ed8-a589-4be2-80a6-f975406eb1f6
         --project_id 2720f785-a3a2-4472-969d-19a53891c817
 
     Environment Variables Required:
@@ -51,7 +52,9 @@ def main() -> int:
             help="Boolean flag for campaign entity caching.\
                     Check if enabled for particular EntityFromID types.",
         )
-        parser.add_argument("--entity_core_api_url", required=True, help="Entity Core API URL as string.")
+        parser.add_argument(
+            "--entity_core_api_url", required=True, help="Entity Core API URL as string."
+        )
         parser.add_argument("--lab_id", required=True, help="Virtual Lab ID as string")
         parser.add_argument("--project_id", required=True, help="Project ID as string.")
 
@@ -61,34 +64,38 @@ def main() -> int:
         L.error(f"Argument parsing error: {e}")
         return 1
 
-    entity_type_str = args.entity_type
-    entity_id = args.entity_id
-    config_asset_id = args.config_asset_id
-    scan_output_root = args.scan_output_root
-    entity_cache = args.entity_cache
-    entity_core_api_url = args.entity_core_api_url
-    lab_id = args.lab_id
-    project_id = args.project_id
+    try:
+        entity_type_str = args.entity_type
+        entity_id = args.entity_id
+        config_asset_id = args.config_asset_id
+        scan_output_root = args.scan_output_root
+        entity_cache = args.entity_cache
+        entity_core_api_url = args.entity_core_api_url
+        lab_id = args.lab_id
+        project_id = args.project_id
 
-    entity_type = getattr(entitysdk.models, entity_type_str)
+        entity_type = getattr(entitysdk.models, entity_type_str)
 
-    token = os.getenv("OBI_AUTHENTICATION_TOKEN")
+        token = os.getenv("OBI_AUTHENTICATION_TOKEN")
 
-    project_context = ProjectContext(virtual_lab_id=lab_id, project_id=project_id)
-    db_client = Client(
-        api_url=entity_core_api_url,
-        token_manager=token,
-        project_context=project_context,
-    )
+        project_context = ProjectContext(virtual_lab_id=lab_id, project_id=project_id)
+        db_client = Client(
+            api_url=entity_core_api_url,
+            token_manager=token,
+            project_context=project_context,
+        )
 
-    run_task_for_single_config_asset(
-        entity_type=entity_type,
-        entity_id=entity_id,
-        config_asset_id=config_asset_id,
-        scan_output_root=scan_output_root,
-        db_client=db_client,
-        entity_cache=entity_cache,
-    )
+        run_task_for_single_config_asset(
+            entity_type=entity_type,
+            entity_id=entity_id,
+            config_asset_id=config_asset_id,
+            scan_output_root=scan_output_root,
+            db_client=db_client,
+            entity_cache=entity_cache,
+        )
+    except OBIONEError as e:
+        L.error(f"Error launching task for single configuration asset: {e}")
+        return 1
 
     return 0
 
