8a8d87cbcdd5a2e15093604c7750b9170715be2a
@darshanmandge <darshanmandge@users.noreply.github.com>
2025-11-11 13:13:36 +0100

    ruff fix to remove comments

--- obi_one/scientific/tasks/ion_channel_modeling.py
+++ obi_one/scientific/tasks/ion_channel_modeling.py
@@ -11,7 +11,7 @@ from typing import Annotated, Any, ClassVar
 
 import entitysdk
 from entitysdk import models
-from entitysdk.types import AssetLabel, ContentType, SimulationExecutionStatus
+from entitysdk.types import AssetLabel, ContentType
 from pydantic import Field, StringConstraints
 
 from obi_one.core.block import Block
@@ -458,17 +458,6 @@ class IonChannelFittingTask(Task):
         )
 
         self.register_plots_and_json(db_client, figure_filepaths, model.id)
-
-        # register the Activity
-        # L.info("-- Register IonChannelModelingExecution Entity")
-        # db_client.register_entity(
-        #     entitysdk.models.IonChannelModelingExecution(
-        #         start_time=datetime.now(UTC),
-        #         status=SimulationExecutionStatus.done,
-        #         used=[self.config.single_entity],
-        #         generated=[model],
-        #     )
-        # )
 
         return model.id
 
