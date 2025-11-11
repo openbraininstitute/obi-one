db5c8af758f31fcd66ccc50936046560c81e8ee7
Daniel Keller <dkeller@daniels-macbook-pro.home>
2025-11-11 07:02:30 +0100

    linting

--- app/application.py
+++ app/application.py
@@ -21,8 +21,8 @@ from app.endpoints import (
     ephys_metrics,
     morphology_metrics,
     morphology_validation,
-    nwb_validation,
     multi_values,
+    nwb_validation,
     scan_config,
 )
 from app.endpoints.scan_config import activate_scan_config_endpoints
@@ -142,5 +142,6 @@ app.include_router(ephys_metrics.router)
 app.include_router(morphology_metrics.router)
 app.include_router(morphology_validation.router)
 app.include_router(multi_values.router)
+app.include_router(nwb_validation.router)
 activate_scan_config_endpoints()
 app.include_router(scan_config.router)
