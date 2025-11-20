cf5440b4c194fe8b4af72f59473f9d28be75443c
Daniel Keller <dkeller@daniels-macbook-pro.home>
2025-11-20 21:42:36 +0100

    linting

--- tests/routers/declared/test_validate_nwb.py
+++ tests/routers/declared/test_validate_nwb.py
@@ -176,7 +176,6 @@ def test_validate_nwb_file_empty(
             "app.endpoints.nwb_validation._save_upload_to_tempfile",
             side_effect=fake_save,
         ),
-
         patch("app.endpoints.nwb_validation._handle_empty_file", side_effect=fake_handle_empty),
         patch("app.endpoints.nwb_validation.validate_all_nwb_readers", mock_validate),
     ):
