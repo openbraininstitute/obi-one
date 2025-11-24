3ec7d25b2686cf34daab2b92f3cbed5ef99adfcb
Christoph Pokorny <christoph.pokorny@openbraininstitute.org>
2025-11-24 17:12:53 +0100

    Added parameterization code

--- obi_one/scientific/tasks/synapse_parameterization.py
+++ obi_one/scientific/tasks/synapse_parameterization.py
@@ -1,6 +1,10 @@
 import logging
 from typing import ClassVar
 
+import bluepysnap as snap
+import h5py
+import pandas as pd
+from connectome_manipulator.model_building import model_types
 from entitysdk import Client, models
 from pydantic import Field, PrivateAttr
 
@@ -26,6 +30,11 @@ class SynapseParameterizationSingleConfig(OBIBaseModel, SingleConfigMixin):
             title="Synaptome",
             description="Synaptome (i.e., circuit of scale single) to (re-)parameterize.",
         )
+        pathway_property: str = Field(
+            title="Pathway property",
+            description="Neuron property (e.g., 'synapse_class') by which to group neurons into"
+            " pathways between source and target neuron populations.",
+        )
         pathway_param_dict: dict = Field(
             title="Pathway parameters",
             description="Synapse physiology distribution parameters for all pathways in the"
@@ -44,6 +53,76 @@ class SynapseParameterizationTask(Task):
     config: SynapseParameterizationSingleConfig
     _synaptome: MEModelWithSynapsesCircuit | None = PrivateAttr(default=None)
     _synaptome_entity: models.Circuit | None = PrivateAttr(default=None)
+    _pathway_model: model_types.ConnPropsModel | None = PrivateAttr(default=None)
+
+    def _wrap_get_model_output(
+        self, cls_src: pd.Series, cls_tgt: pd.Series, row: pd.Series
+    ) -> pd.DataFrame:
+        src_type = cls_src[row["source"]]
+        tgt_type = cls_tgt[row["target"]]
+        idx = row["index"]
+
+        mdl = self._pathway_model.get_model_output(
+            src_type=src_type, tgt_type=tgt_type, n_syn=len(idx)
+        )
+        mdl["_index"] = idx
+        return mdl
+
+    def _parameterize_edge_file(self, edge: snap.edges.EdgePopulation) -> None:
+        # Get pathway source/target values
+        pathway_property = self.config.initialize.pathway_property
+        if pathway_property not in edge.source.property_names:
+            msg = (
+                f"Pathway property '{pathway_property}' not found in source nodes:"
+                " Skipping edge population '{edge.name}'!"
+            )
+            L.warning(msg)
+            return
+        if pathway_property not in edge.target.property_names:
+            msg = (
+                f"Pathway property '{pathway_property}' not found in target nodes:"
+                " Skipping edge population '{edge.name}'!"
+            )
+            L.warning(msg)
+            return
+        cls_src = edge.source.get(properties=pathway_property)
+        cls_tgt = edge.target.get(properties=pathway_property)
+
+        # Open edge file
+        edge_prefix = f"edges/{edge.name}"
+        with h5py.File(edge.h5_filepath, "a") as h5:
+            edge_grp = h5[edge_prefix]
+
+            # Get connectivity
+            src_ids = edge_grp["source_node_id"]
+            tgt_ids = edge_grp["target_node_id"]
+            src_tgt_df = pd.DataFrame(
+                {"source": src_ids, "target": tgt_ids, "index": range(len(src_ids))}
+            )
+            src_tgt_df = src_tgt_df.groupby(["source", "target"])["index"].apply(list).reset_index()
+
+            # Draw values
+            drawn_values = [
+                self._wrap_get_model_output(cls_src, cls_tgt, src_tgt_df.iloc[i])
+                for i in range(len(src_tgt_df))
+            ]
+            new_props = pd.concat(drawn_values, axis=0).set_index("_index", drop=True).sort_index()
+            for col in new_props.columns:
+                new_values = new_props[col].to_numpy()
+                if col in edge_grp["0"]:
+                    msg = (
+                        f"Synapse property '{col}' already exists in edge population"
+                        " '{edge.name}': "
+                    )
+                    if self.config.initialize.overwrite_if_exists:
+                        msg += "Re-parameterizing synapses."
+                        L.info(msg)
+                        edge_grp["0"][col][...] = new_values
+                    else:
+                        msg += "Choose 'overwrite' to re-parameterize synapses!"
+                        raise ValueError(msg)
+                else:
+                    edge_grp["0"].create_dataset(col, data=new_values)
 
     def execute(self, *, db_client: Client = None, entity_cache: bool = False) -> None:
         if db_client is None:
@@ -52,9 +131,44 @@ class SynapseParameterizationTask(Task):
 
         # Stage synaptome
         self._synaptome_entity = self.config.initialize.synaptome.entity(db_client=db_client)
-        # self._synaptome = self.config.initialize.synaptome.stage_circuit(
-        #     db_client=db_client, dest_dir=circuit_dest_dir, entity_cache=entity_cache
-        # )
+        output_dir = self.config.coordinate_output_root.resolve()
+        self._synaptome = self.config.initialize.synaptome.stage_circuit(
+            db_client=db_client, dest_dir=output_dir, entity_cache=entity_cache
+        )
 
-        L.info("Running synapse parameterization...")
-        L.error("Not yet implemented!")
+        # Check parameters
+        circ = self._synaptome.sonata_circuit
+        pathway_property = self.config.initialize.pathway_property
+        if pathway_property not in circ.nodes.property_names:
+            msg = f"Unknown pathway property '{pathway_property}'!"
+            raise ValueError(msg)
+        type_values = circ.nodes.property_values(pathway_property)
+        if not all(
+            _t in self.config.initialize.pathway_param_dict.get("src_types", [])
+            for _t in type_values
+        ):
+            msg = f"Source type(s) missing in pathway parameter dict! Must contain: {type_values}"
+            raise ValueError(msg)
+        if not all(
+            _t in self.config.initialize.pathway_param_dict.get("tgt_types", [])
+            for _t in type_values
+        ):
+            msg = f"Target type(s) missing in pathway parameter dict! Must contain: {type_values}"
+            raise ValueError(msg)
+
+        # Initialize pathway parameter model
+        self._pathway_model = model_types.ConnPropsModel(
+            **self.config.initialize.pathway_param_dict
+        )
+        model_str = str(self._pathway_model)
+        model_str = model_str.replace("M-types:", f"'{pathway_property}' pathways:")
+        L.info(model_str)
+
+        # Run parameterization
+        edge_pop_names = circ.edges.population_names
+        L.info(f"Running synapse parameterization for {len(edge_pop_names)} edge population(s)...")
+        for edge_pop in edge_pop_names:
+            edge = circ.edges[edge_pop]
+            self._parameterize_edge_file(edge)
+
+        # TODO: Register (re-)parameterized synaptome
