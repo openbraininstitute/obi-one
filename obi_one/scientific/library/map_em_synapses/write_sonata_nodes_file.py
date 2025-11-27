7ebe99c37fe85fc65afd891d2cbc257c4fd9211f
Michael W. Reimann <michael.reimann@openbraininstitute.org>
2025-11-27 10:35:43 +0100

    Updated the check for bool types

--- obi_one/scientific/library/map_em_synapses/write_sonata_nodes_file.py
+++ obi_one/scientific/library/map_em_synapses/write_sonata_nodes_file.py
@@ -59,7 +59,7 @@ def assemble_collection_from_specs(
     out_df.index = pandas.Index(range(1, len(out_df) + 1))
 
     for col in out_df.columns:
-        if isinstance(out_df[col].dtype, pandas.BooleanDtype):
+        if out_df[col].dtype in ("bool", "boolean"):  # NOQA: PLR6201
             out_df[col] = out_df[col].astype(str)
 
     return voxcell.CellCollection.from_dataframe(out_df), lst_notices
