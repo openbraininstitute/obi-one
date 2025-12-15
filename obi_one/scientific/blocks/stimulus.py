6a0d0b04bc9a73bf9f8ec903fea846595f8de512
James Isbister <isbisterjb@gmail.com>
2025-12-15 20:15:04 +0100

    4 OU stimuli added

--- obi_one/scientific/blocks/stimulus.py
+++ obi_one/scientific/blocks/stimulus.py
@@ -564,6 +564,276 @@ class HyperpolarizingCurrentClampSomaticStimulus(SomaticStimulus):
                 "represents_physical_electrode": self._represents_physical_electrode,
             }
         return sonata_config
+    
+
+    class OrnsteinUhlenbeckCurrentSomaticStimulus(SomaticStimulus):
+        """A current injection based on the Ornstein-Uhlenbeck process."""
+
+        title: ClassVar[str] = "Ornstein-Uhlenbeck Current Clamp (Absolute)"
+
+        _module: str = "ornstein_uhlenbeck"
+        _input_type: str = "current"
+
+        time_constant: PositiveFloat | list[PositiveFloat] = Field(
+            default=5.0,
+            title="Tau",
+            description="The time constant of the Ornstein-Uhlenbeck process.",
+            units="ms",
+        )
+
+        mean_amplitude: NonNegativeFloat | list[NonNegativeFloat] = Field(
+            default=0.1,
+            title="Mean Amplitude",
+            description="The mean value of current to inject. Given in nanoamps (nA).",
+            units="nA",
+        )
+
+        standard_deviation: PositiveFloat | list[PositiveFloat] = Field(
+            default=0.05,
+            title="Standard Deviation",
+            description="The standard deviation of current to inject. Given in nanoamps (nA).",
+            units="nA",
+        )
+
+        dt: (
+            Annotated[NonNegativeFloat, Field(ge=_MIN_TIME_STEP_MILLISECONDS)]
+            | list[Annotated[NonNegativeFloat, Field(ge=_MIN_TIME_STEP_MILLISECONDS)]]
+        ) = Field(
+            default=0.025,
+            description="Timestep of generated signal in milliseconds (ms).",
+            title="Timestep",
+            units="ms",
+        )
+
+        def _generate_config(self) -> dict:
+            sonata_config = {}
+
+            timestamps_block = resolve_timestamps_ref_to_timestamps_block(
+                self.timestamps, self._default_timestamps
+            )
+
+            for t_ind, timestamp in enumerate(timestamps_block.timestamps()):
+                sonata_config[self.block_name + "_" + str(t_ind)] = {
+                    "delay": timestamp + self.timestamp_offset,
+                    "duration": self.duration,
+                    "node_set": resolve_neuron_set_ref_to_node_set(
+                        self.neuron_set, self._default_node_set
+                    ),
+                    "module": self._module,
+                    "input_type": self._input_type,
+                    "tau": self.time_constant,
+                    "mean": self.mean_amplitude,
+                    "sigma": self.standard_deviation,
+                    "dt": self.dt,
+                    "represents_physical_electrode": self._represents_physical_electrode,
+                }
+            return sonata_config
+        
+    class OrnsteinUhlenbeckConductanceSomaticStimulus(SomaticStimulus):
+        """A conductance injection based on the Ornstein-Uhlenbeck process."""
+
+        title: ClassVar[str] = "Ornstein-Uhlenbeck Conductance Clamp (Absolute)"
+
+        _module: str = "ornstein_uhlenbeck"
+        _input_type: str = "conductance"
+
+        time_constant: PositiveFloat | list[PositiveFloat] = Field(
+            default=5.0,
+            title="Tau",
+            description="The time constant of the Ornstein-Uhlenbeck process.",
+            units="ms",
+        )
+
+        mean_amplitude: NonNegativeFloat | list[NonNegativeFloat] = Field(
+            default=0.1,
+            title="Mean Amplitude",
+            description="The mean value of conductance to inject. Given in nanosiemens (nS).",
+            units="nS",
+        )
+
+        standard_deviation: PositiveFloat | list[PositiveFloat] = Field(
+            default=0.05,
+            title="Standard Deviation",
+            description="The standard deviation of conductance to inject. Given in nanosiemens (nS).",
+            units="nS",
+        )
+
+        reversal_potential: float | list[float] = Field(
+            default=0.0,
+            title="Reversal Potential",
+            description="The reversal potential of the conductance injection.",
+            units="mV",
+        )
+
+        dt: (
+            Annotated[NonNegativeFloat, Field(ge=_MIN_TIME_STEP_MILLISECONDS)]
+            | list[Annotated[NonNegativeFloat, Field(ge=_MIN_TIME_STEP_MILLISECONDS)]]
+        ) = Field(
+            default=0.025,
+            description="Timestep of generated signal in milliseconds (ms).",
+            title="Timestep",
+            units="ms",
+        )
+
+        def _generate_config(self) -> dict:
+            sonata_config = {}
+
+            timestamps_block = resolve_timestamps_ref_to_timestamps_block(
+                self.timestamps, self._default_timestamps
+            )
+
+            for t_ind, timestamp in enumerate(timestamps_block.timestamps()):
+                sonata_config[self.block_name + "_" + str(t_ind)] = {
+                    "delay": timestamp + self.timestamp_offset,
+                    "duration": self.duration,
+                    "node_set": resolve_neuron_set_ref_to_node_set(
+                        self.neuron_set, self._default_node_set
+                    ),
+                    "module": self._module,
+                    "input_type": self._input_type,
+                    "tau": self.time_constant,
+                    "mean": self.mean_amplitude,
+                    "sigma": self.standard_deviation,
+                    "reversal": self.reversal_potential,
+                    "dt": self.dt,
+                    "represents_physical_electrode": self._represents_physical_electrode,
+                }
+            return sonata_config
+    
+    class RelativeOrnsteinUhlenbeckCurrentSomaticStimulus(SomaticStimulus):
+        """A current injection based on the Ornstein-Uhlenbeck process as a percentage of each cell's threshold current."""
+
+        title: ClassVar[str] = "Ornstein-Uhlenbeck Current Clamp (Relative)"
+
+        _module: str = "ornstein_uhlenbeck"
+        _input_type: str = "current"
+
+        time_constant: PositiveFloat | list[PositiveFloat] = Field(
+            default=5.0,
+            title="Tau",
+            description="The time constant of the Ornstein-Uhlenbeck process.",
+            units="ms",
+        )
+
+        mean_percentage_of_threshold_current: NonNegativeFloat | list[NonNegativeFloat] = Field(
+            default=100.0,
+            title="Mean Percentage of Threshold Current",
+            description="Signal mean as percentage of a cell's threshold current.",
+            units="%",
+        )
+
+        standard_deviation_percentage_of_threshold: PositiveFloat | list[PositiveFloat] = Field(
+            default=5.0,
+            title="Standard Deviation",
+            description="Signal standard deviation as percentage of a cell's threshold current.",
+            units="%",
+        )
+
+        dt: (
+            Annotated[NonNegativeFloat, Field(ge=_MIN_TIME_STEP_MILLISECONDS)]
+            | list[Annotated[NonNegativeFloat, Field(ge=_MIN_TIME_STEP_MILLISECONDS)]]
+        ) = Field(
+            default=0.025,
+            description="Timestep of generated signal in milliseconds (ms).",
+            title="Timestep",
+            units="ms",
+        )
+
+        def _generate_config(self) -> dict:
+            sonata_config = {}
+
+            timestamps_block = resolve_timestamps_ref_to_timestamps_block(
+                self.timestamps, self._default_timestamps
+            )
+
+            for t_ind, timestamp in enumerate(timestamps_block.timestamps()):
+                sonata_config[self.block_name + "_" + str(t_ind)] = {
+                    "delay": timestamp + self.timestamp_offset,
+                    "duration": self.duration,
+                    "node_set": resolve_neuron_set_ref_to_node_set(
+                        self.neuron_set, self._default_node_set
+                    ),
+                    "module": self._module,
+                    "input_type": self._input_type,
+                    "tau": self.time_constant,
+                    "mean_percent": self.mean_percentage_of_threshold_current,
+                    "sd_percent": self.standard_deviation_percentage_of_threshold,
+                    "dt": self.dt,
+                    "represents_physical_electrode": self._represents_physical_electrode,
+                }
+            return sonata_config
+        
+
+    class RelativeOrnsteinUhlenbeckConductanceSomaticStimulus(SomaticStimulus):
+        """A conductance injection based on the Ornstein-Uhlenbeck process as a percentage of each cell's input conductance (inverse of the input resistance)."""
+
+        title: ClassVar[str] = "Ornstein-Uhlenbeck Conductance Clamp (Relative)"
+
+        _module: str = "ornstein_uhlenbeck"
+        _input_type: str = "conductance"
+
+        time_constant: PositiveFloat | list[PositiveFloat] = Field(
+            default=5.0,
+            title="Tau",
+            description="The time constant of the Ornstein-Uhlenbeck process.",
+            units="ms",
+        )
+
+        mean_percentage_of_cells_input_conductance: NonNegativeFloat | list[NonNegativeFloat] = Field(
+            default=100.0,
+            title="Mean Percentage of Cells' Input Conductance",
+            description="Signal mean as percentage of a cell's input conductance.",
+            units="%",
+        )
+
+        standard_deviation_percentage_of_cells_input_conductance: PositiveFloat | list[PositiveFloat] = Field(
+            default=5.0,
+            title="Standard Deviation",
+            description="Signal standard deviation as percentage of a cell's input conductance.",
+            units="%",
+        )
+
+        reversal_potential: float | list[float] = Field(
+            default=0.0,
+            title="Reversal Potential",
+            description="The reversal potential of the conductance injection.",
+            units="mV",
+        )
+
+        dt: (
+            Annotated[NonNegativeFloat, Field(ge=_MIN_TIME_STEP_MILLISECONDS)]
+            | list[Annotated[NonNegativeFloat, Field(ge=_MIN_TIME_STEP_MILLISECONDS)]]
+        ) = Field(
+            default=0.025,
+            description="Timestep of generated signal in milliseconds (ms).",
+            title="Timestep",
+            units="ms",
+        )
+
+        def _generate_config(self) -> dict:
+            sonata_config = {}
+
+            timestamps_block = resolve_timestamps_ref_to_timestamps_block(
+                self.timestamps, self._default_timestamps
+            )
+
+            for t_ind, timestamp in enumerate(timestamps_block.timestamps()):
+                sonata_config[self.block_name + "_" + str(t_ind)] = {
+                    "delay": timestamp + self.timestamp_offset,
+                    "duration": self.duration,
+                    "node_set": resolve_neuron_set_ref_to_node_set(
+                        self.neuron_set, self._default_node_set
+                    ),
+                    "module": self._module,
+                    "input_type": self._input_type,
+                    "tau": self.time_constant,
+                    "mean_percent": self.mean_percentage_of_cells_input_conductance,
+                    "sd_percent": self.standard_deviation_percentage_of_cells_input_conductance,
+                    "dt": self.dt,
+                    "reversal": self.reversal_potential,
+                    "represents_physical_electrode": self._represents_physical_electrode,
+                }
+            return sonata_config
 
 
 class SpikeStimulus(Stimulus):
