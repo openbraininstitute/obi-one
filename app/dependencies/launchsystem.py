027915222cfd578dd7ad86781e8467557357018f
Christoph Pokorny <christoph.pokorny@openbraininstitute.org>
2026-01-12 17:45:22 +0100

    ADded LAUNCH_SYSTEM_URL to settings

--- app/dependencies/launchsystem.py
+++ app/dependencies/launchsystem.py
@@ -7,10 +7,7 @@ from app.dependencies.auth import UserContextDep
 def get_client(
     user_context: UserContextDep,
 ) -> httpx.Client:
-    api_url = settings.ENTITYCORE_URL.replace(
-        "entitycore", "launch-system"
-    )  # TODO: Create separate entry in settings (?)
-
+    api_url = settings.LAUNCH_SYSTEM_URL
     token = user_context.token.credentials
     client = httpx.Client(base_url=api_url, headers={"Authorization": f"Bearer {token}"})
 
