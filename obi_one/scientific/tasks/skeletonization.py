928dc687f78fe60db09981a3288dde31598667c6
james-isbister <isbisterjb@gmail.com>
2026-01-28 15:04:11 +0100

    Change root element names (#554)
    * root_block -> block_single
    * block_single -> block_union
    * Changed in package, docs and tests
    * Also fixed test to use `validate_block_single` (previously `validate_root_block` which wasn't getting used)

--- obi_one/scientific/tasks/skeletonization.py
+++ obi_one/scientific/tasks/skeletonization.py
@@ -81,7 +81,7 @@ class SkeletonizationScanConfig(ScanConfig, abc.ABC):
         )
 
     info: Info = Field(
-        ui_element="root_block",
+        ui_element="block_single",
         title="Info",
         description="Information about the skeletonization campaign.",
         group=BlockGroup.SETUP_BLOCK_GROUP,
@@ -89,7 +89,7 @@ class SkeletonizationScanConfig(ScanConfig, abc.ABC):
     )
 
     initialize: Initialize = Field(
-        ui_element="root_block",
+        ui_element="block_single",
         title="Initialization",
         description="Parameters for initializing the skeletonization.",
         group=BlockGroup.SETUP_BLOCK_GROUP,
