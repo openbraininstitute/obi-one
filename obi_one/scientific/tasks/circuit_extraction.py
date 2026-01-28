d31076e039bb98df0a7d3e9b2513f257d3dd5634
James Isbister <isbisterjb@gmail.com>
2026-01-28 15:56:37 +0100

    merge fixes

--- obi_one/scientific/tasks/circuit_extraction.py
+++ obi_one/scientific/tasks/circuit_extraction.py
@@ -63,57 +63,63 @@ class CircuitExtractionScanConfig(ScanConfig):
     model_config = ConfigDict(
         json_schema_extra={
             "ui_enabled": True,
-            "block_block_group_order": [BlockGroup.SETUP, BlockGroup.EXTRACTION_TARGE],
+            "group_order": [BlockGroup.SETUP, BlockGroup.EXTRACTION_TARGET],
         }
     )
 
     class Initialize(Block):
         circuit: CircuitDiscriminator | list[CircuitDiscriminator] = Field(
-            ui_element="model_identifier",
             title="Circuit",
             description="Parent circuit to extract a sub-circuit from.",
+            json_schema_extra={
+                "ui_element": "model_identifier",
+            }
         )
         do_virtual: bool = Field(
-            ui_element="boolean_input",
             default=True,
             title="Include Virtual Populations",
             description="Include virtual neurons which target the cells contained in the specified"
             " neuron set (together with their connectivity onto the specified neuron set) in the"
             " extracted sub-circuit.",
+            json_schema_extra={
+                "ui_element": "boolean_input",
+            }
         )
         create_external: bool = Field(
-            ui_element="boolean_input",
             default=True,
             title="Create External Population",
             description="Convert (non-virtual) neurons which are outside of the specified neuron"
             " set, but which target the cells contained therein, into a new external population"
             " of virtual neurons (together with their connectivity onto the specified neuron set).",
+            json_schema_extra={
+                "ui_element": "boolean_input",
+            }
         )
 
     info: Info = Field(
-        ui_element="block_single",
         title="Info",
         description="Information about the circuit extraction campaign.",
         json_schema_extra={
+            "ui_element": "block_single",
             "group": BlockGroup.SETUP,
             "group_order": 0,
         },
     )
     initialize: Initialize = Field(
-        ui_element="block_single",
         title="Initialization",
         description="Parameters for initializing the circuit extraction campaign.",
         json_schema_extra={
+            "ui_element": "block_single",
             "group": BlockGroup.SETUP,
             "group_order": 1,
         },
     )
     neuron_set: CircuitExtractionNeuronSetUnion = Field(
-        ui_element="block_union",
         title="Neuron Set",
         description="Set of neurons to be extracted from the parent circuit, including their"
         " connectivity.",
         json_schema_extra={
+            "ui_element": "block_union",
             "group": BlockGroup.EXTRACTION_TARGET,
             "group_order": 0,
         },
