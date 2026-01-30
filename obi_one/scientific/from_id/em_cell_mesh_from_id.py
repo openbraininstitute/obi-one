1a5326e9c8f1d6de71b8dff0f2562235c9c6f4a7
Michael W. Reimann <michael.reimann@openbraininstitute.org>
2026-01-30 09:59:30 +0100

    Added methods to resolve em entities The following queries have been added to the respective _from_id classes: - For a dense reconstruction dataset: all its meshes - For a dense reconstruction dataset: all its skeletonized morphologies - For a em cell mesh: all its skeletonized morphologies

--- obi_one/scientific/from_id/em_cell_mesh_from_id.py
+++ obi_one/scientific/from_id/em_cell_mesh_from_id.py
@@ -1,11 +1,42 @@
 from typing import ClassVar
 
 import entitysdk
+from entitysdk import Client
+from entitysdk.models import SkeletonizationExecution
 from pydantic import PrivateAttr
+from uuid import UUID
 
 from obi_one.core.entity_from_id import EntityFromID
+from obi_one.scientific.from_id.cell_morphology_from_id import CellMorphologyFromID
 
 
 class EMCellMeshFromID(EntityFromID):
     entitysdk_class: ClassVar[type[entitysdk.models.entity.Entity]] = entitysdk.models.EMCellMesh
     _entity: entitysdk.models.EMCellMesh | None = PrivateAttr(default=None)
+
+    def cell_morphology_ids(
+        self, db_client: Client | None = None, only_project: bool = False
+    ) -> list[UUID]:
+        query = db_client.search_entity(
+            entity_type=SkeletonizationExecution,
+            query= {
+                "used__id": self.id_str
+            }
+        )
+        morphology_ids = []
+        for activity in query:
+            for generated_morph in activity.generated:
+                if (not generated_morph.authorized_public) or (not only_project):
+                    morphology_ids.append(generated_morph.id)
+        return morphology_ids
+    
+    def cell_morphologies(
+        self, db_client: Client | None = None, only_project: bool = False
+    ) -> list[CellMorphologyFromID]:
+        morph_ids = self.cell_morphology_ids(
+            db_client=db_client, only_project=only_project
+        )
+        morph_from_ids = [
+            CellMorphologyFromID(id_str=str(id_)) for id_ in morph_ids
+        ]
+        return morph_from_ids
--- obi_one/scientific/from_id/em_dataset_from_id.py
+++ obi_one/scientific/from_id/em_dataset_from_id.py
@@ -4,11 +4,14 @@ import numpy  # NOQA: ICN001
 import pandas  # NOQA: ICN001
 from caveclient import CAVEclient
 from entitysdk import Client
-from entitysdk.models import EMDenseReconstructionDataset
+from entitysdk.models import EMDenseReconstructionDataset, EMCellMesh, CellMorphology, SkeletonizationExecution
 from entitysdk.models.entity import Entity
 from pydantic import PrivateAttr
+from uuid import UUID
 
 from obi_one.core.entity_from_id import EntityFromID
+from obi_one.scientific.from_id.cell_morphology_from_id import CellMorphologyFromID
+from obi_one.scientific.from_id.em_cell_mesh_from_id import EMCellMeshFromID
 
 _C_P_LOCS = ["synapse_x", "synapse_y", "synapse_z"]
 _NM_to_UM = 1e-3
@@ -58,6 +61,65 @@ class EMDataSetFromID(EntityFromID):
 
         notice_text = client.materialize.get_table_metadata(table_name).get("notice_text")
         return tbl, notice_text
+    
+    def cell_mesh_ids(
+        self, db_client: Client | None = None, limit: int = 100, only_project: bool = False
+    ) -> list[UUID]:
+        query = {
+            "em_dense_reconstruction_dataset__id": self.id_str,
+        }
+        if only_project:
+            query["authorized_public"] = False
+
+        entities = db_client.search_entity(entity_type=EMCellMesh,
+            query=query, limit=limit
+        )
+        entity_ids = [entity_.id for entity_ in entities]
+        return entity_ids
+    
+    def cell_meshes(
+        self, db_client: Client | None = None, limit: int = 100, only_project: bool = False
+    ) -> list[EMCellMeshFromID]:
+        mesh_ids = self.cell_mesh_ids(
+            db_client=db_client, limit=limit, only_project=only_project
+        )
+        mesh_from_ids = [
+            EMCellMeshFromID(id_str=str(id_)) for id_ in mesh_ids
+        ]
+        return mesh_from_ids
+    
+    def cell_morphology_ids(
+        self, db_client: Client | None = None, limit: int = 100, only_project: bool = False
+    ) -> list[UUID]:
+        chunk_sz = 20
+        mesh_ids = self.cell_mesh_ids(db_client=db_client, limit=limit)
+
+        morphology_ids = []
+        chunks = numpy.arange(0, len(mesh_ids) + chunk_sz, chunk_sz)
+
+        for i, j in zip(chunks[:-1], chunks[1:]):
+            query = db_client.search_entity(
+                entity_type=SkeletonizationExecution,
+                query= {
+                    "used__id__in": mesh_ids[i:j]
+                }
+            )
+            for activity in query:
+                for generated_morph in activity.generated:
+                    if (not generated_morph.authorized_public) or (not only_project):
+                        morphology_ids.append(generated_morph.id)
+        return morphology_ids
+    
+    def cell_morphologies(
+        self, db_client: Client | None = None, limit: int = 100, only_project: bool = False
+    ) -> list[CellMorphologyFromID]:
+        morph_ids = self.cell_morphology_ids(
+            db_client=db_client, limit=limit, only_project=only_project
+        )
+        morph_from_ids = [
+            CellMorphologyFromID(id_str=str(id_)) for id_ in morph_ids
+        ]
+        return morph_from_ids
 
     def get_versions(self, db_client: Client | None = None) -> list:
         client = self._make_cave_client(db_client)
