187fe3d9e0c451e42aabd69794097bd1599317ad
James Isbister <isbisterjb@gmail.com>
2026-02-02 11:27:39 +0100

    Linting

--- obi_one/scientific/tasks/schema_example.py
+++ obi_one/scientific/tasks/schema_example.py
@@ -1,7 +1,7 @@
 from enum import StrEnum
 from typing import ClassVar, Literal
 
-from pydantic import Field, ConfigDict
+from pydantic import ConfigDict, Field
 
 from obi_one.core.block import Block
 from obi_one.core.info import Info
