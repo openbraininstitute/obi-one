0dff1f7ad2e1839b04a1775debae0c837c00a067
James Isbister <isbisterjb@gmail.com>
2026-02-13 22:24:29 +0100

    Use model_config

--- obi_one/scientific/blocks/stimuli/electric_field.py
+++ obi_one/scientific/blocks/stimuli/electric_field.py
@@ -1,7 +1,7 @@
 from typing import Annotated, ClassVar, Self
 
 import numpy as np
-from pydantic import Field, NonNegativeFloat, PrivateAttr, model_validator
+from pydantic import ConfigDict, Field, NonNegativeFloat, PrivateAttr, model_validator
 
 from obi_one.scientific.library.constants import (
     _DEFAULT_STIMULUS_LENGTH_MILLISECONDS,
@@ -17,10 +17,13 @@ from .stimulus import Stimulus
 class SpatiallyUniformElectricFieldStimulus(Stimulus):
     """A uniform electric field stimulus applied to all compartments of biophysical neurons."""
 
-    json_schema_extra = {
-        "entity_property_requirement": {"scale": ["small_microcircuit"]},
-        "entity_property_unfulfilled_message": "This stimulus is currently only supported for microcircuits.",
-    }
+    model_config = ConfigDict(
+        json_schema_extra={
+            "entity_property_requirement": {"scale": ["small_microcircuit"]},
+            "entity_property_unfulfilled_message": "This stimulus is currently only "
+            "supported for microcircuits.",
+        }
+    )
 
     title: ClassVar[str] = "Uniform Electric Field"
 
