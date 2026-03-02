2e71c071de3dcc2fabb1170f40733c9af32ca434
James Isbister <isbisterjb@gmail.com>
2026-03-02 20:15:23 +0100

    Start model_selector_single

--- obi_one/scientific/blocks/ion_channel_model.py
+++ obi_one/scientific/blocks/ion_channel_model.py
@@ -1,22 +1,24 @@
-from abc import ABC
-
 from pydantic import Field, NonNegativeFloat
 
 from obi_one.core.block import Block
 from obi_one.scientific.from_id.ion_channel_model_from_id import IonChannelModelFromID
 
 
-class IonChannelModel(Block, ABC):
+
+class IonChannelModelWithConductance(Block):
+
     ion_channel_model: IonChannelModelFromID = Field(
         title="Ion channel model",
         description="ID of the model to simulate.",
         json_schema_extra={
-            "ui_element": "model_identifier",
+            "ui_element": "model_selector",
+            "model_selector_entity_type": "IonChannelModel",
+            "model_selector_property_filter": {
+                "conductance_name": not None,
+            }
         },
     )
 
-
-class IonChannelModelWithConductance(IonChannelModel):
     conductance: NonNegativeFloat | list[NonNegativeFloat] = Field(
         title="Conductance value (in S/cm2)",
         description="Conductance value (in S/cm2).",
@@ -28,6 +30,20 @@ class IonChannelModelWithConductance(IonChannelModel):
 
 
 class IonChannelModelWithMaxPermeability(IonChannelModel):
+
+    ion_channel_model: IonChannelModelFromID = Field(
+        title="Ion channel model",
+        description="ID of the model to simulate.",
+        json_schema_extra={
+            "ui_element": "model_selector",
+            "model_selector_entity_type": "IonChannelModel",
+            "model_selector_property_filter": {
+                "max_permeability_name": not None,
+            }
+        },
+    )
+
+
     max_permeability: NonNegativeFloat | list[NonNegativeFloat] = Field(
         title="Maximum permeability value (in cm/s)",
         description="Maximum permeability value (in cm/s).",
