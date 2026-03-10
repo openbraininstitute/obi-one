a8a4494c15e08d9d7c190a15f4ca2723d71207c8
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2026-03-10 16:56:31 +0100

    models

--- app/services/circuit_visualization.py
+++ app/services/circuit_visualization.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 from pathlib import Path
-from typing import Annotated, cast
+from typing import cast
 from uuid import UUID
 
 import h5py
@@ -12,54 +12,10 @@ from entitysdk.exception import EntitySDKError
 from entitysdk.models import Circuit
 from entitysdk.types import CircuitScale
 from fastapi import HTTPException
-from pydantic import BaseModel
 
 from app.errors import ApiErrorCode
 from app.logger import L
-
-PositionVector = Annotated[tuple[float, float, float], "x", "y", "z"]
-OrientationVector = Annotated[tuple[float, float, float, float], "x", "y", "z", "w"]
-
-
-class Node(BaseModel):
-    morphology_path: str  # Path to the morphology in the circuit's sonata directory
-    position: PositionVector
-    orientation: OrientationVector
-    soma_radius: float | None
-
-
-Nodes = list[Node]
-
-
-class NeuronSectionInfo(BaseModel):
-    index: int
-    name: str
-    nseg: int
-    distance_from_soma: float
-    sec_length: float
-    xstart: list[float]
-    xend: list[float]
-    xcenter: list[float]
-    xdirection: list[float]
-    ystart: list[float]
-    yend: list[float]
-    ycenter: list[float]
-    ydirection: list[float]
-    zstart: list[float]
-    zend: list[float]
-    zcenter: list[float]
-    zdirection: list[float]
-    diam: list[float]
-    length: list[float]
-    distance: list[float]
-    segment_distance_from_soma: list[float]
-    segx: list[float]
-    neuron_section_id: int
-    neuron_segments_offset: list[int]
-    parent_index: int
-
-
-Morphology = dict[str, NeuronSectionInfo]
+from app.schemas.visualization import Morphology, Node, Nodes, OrientationVector, PositionVector
 
 
 def circuit_asset_id(client: Client, circuit_id: UUID) -> UUID:
