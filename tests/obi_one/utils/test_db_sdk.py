c7d345073c54b6a45fd3555cbeced418fb59448b
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2026-03-20 15:30:58 +0100

    Add ICM Simulation Support via Task Manager (#621)

--- tests/obi_one/utils/test_db_sdk.py
+++ tests/obi_one/utils/test_db_sdk.py
@@ -9,7 +9,7 @@ import pytest
 from entitysdk.models import Asset, Entity, SimulationExecution
 from entitysdk.types import AssetLabel, ExecutorType
 
-from app.utils import db_sdk as test_module
+from obi_one.utils import db_sdk as test_module
 
 
 @pytest.fixture
@@ -41,9 +41,9 @@ def mock_entity_with_assets():
     return config
 
 
-def test_get_config_asset(client, mock_entity_with_assets):
+def test_get_entity_asset_by_label(client, mock_entity_with_assets):
     """Test successful retrieval of config asset"""
-    result = test_module.get_config_asset(
+    result = test_module.get_entity_asset_by_label(
         client=client,
         config=mock_entity_with_assets,
         asset_label=AssetLabel.morphology,
