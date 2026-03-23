28f5e08a1e4c7b50956553388f3d7ee3c845cc61
James Isbister <isbisterjb@gmail.com>
2026-03-23 21:11:55 +0100

    Working changes

--- obi_one/scientific/blocks/synaptic_models/tsodyks_markram.py
+++ obi_one/scientific/blocks/synaptic_models/tsodyks_markram.py
@@ -6,6 +6,7 @@ from pydantic import Field
 
 from obi_one.core.block import Block
 from obi_one.core.schema import SchemaKey, UIElement
+from obi_one.core.units import Units
 from obi_one.scientific.unions.unions_distributions import (
     AllDistributionsReference,
 )
@@ -24,6 +25,63 @@ class TsodyksMarkramSynapticModel(Block):
         },
     )
 
+    conductance_distribution: AllDistributionsReference = Field(
+        title="Conductance (g_syn) Distribution",
+        description="Distribution of synaptic conductance (g_syn).",
+        json_schema_extra={
+            SchemaKey.UI_ELEMENT: UIElement.REFERENCE,
+            SchemaKey.REFERENCE_TYPE: AllDistributionsReference.__name__,
+        },
+    )
+
+    fascilitation_time: AllDistributionsReference = Field(
+        title="Fascilitation Time Distribution",
+        description="Fascilitation Time Distribution",
+        json_schema_extra={
+            SchemaKey.UI_ELEMENT: UIElement.REFERENCE,
+            SchemaKey.REFERENCE_TYPE: AllDistributionsReference.__name__,
+            SchemaKey.UNITS: Units.MILLISECONDS
+        },
+    )
+
+    depression_time: AllDistributionsReference = Field(
+        title="Depression Time Distribution",
+        description="Depression Time Distribution",
+        json_schema_extra={
+            SchemaKey.UI_ELEMENT: UIElement.REFERENCE,
+            SchemaKey.REFERENCE_TYPE: AllDistributionsReference.__name__,
+            SchemaKey.UNITS: Units.MILLISECONDS
+        },
+    )
+
+    n_rrp_vesicles_distribution: AllDistributionsReference = Field(
+        title="Number of RRP Vesicles Distribution",
+        description="Distribution of the number of readily releasable pool (RRP) vesicles.",
+        json_schema_extra={
+            SchemaKey.UI_ELEMENT: UIElement.REFERENCE,
+            SchemaKey.REFERENCE_TYPE: AllDistributionsReference.__name__,
+        },
+    )
+
+    decay_time: AllDistributionsReference = Field(
+        title="Decay Time Distribution",
+        description="Decay Time Distribution",
+        json_schema_extra={
+            SchemaKey.UI_ELEMENT: UIElement.REFERENCE,
+            SchemaKey.REFERENCE_TYPE: AllDistributionsReference.__name__,
+            SchemaKey.UNITS: Units.MILLISECONDS
+        },
+    )
+
+    usyn: AllDistributionsReference = Field(
+        title="Usyn Distribution",
+        description="Distribution of the utilization of synaptic efficacy (usyn) for the first spike in a spike train.",
+        json_schema_extra={
+            SchemaKey.UI_ELEMENT: UIElement.REFERENCE,
+            SchemaKey.REFERENCE_TYPE: AllDistributionsReference.__name__,
+        },
+    )
+
     u_hill_coefficient_shared_within: bool = Field(
         default=False,
         title="U Hill Coefficient Shared Within",
@@ -35,19 +93,50 @@ class TsodyksMarkramSynapticModel(Block):
         },
     )
 
-    gsyn_distribution: AllDistributionsReference = Field(
-        title="g_syn Distribution",
-        description="Distribution of synaptic conductance (g_syn).",
+    conductance_distribution_shared_within: bool = Field(
+        default=False,
+        title="Conductance (g_syn) Distribution Shared Within",
+        description="Whether the synaptic conductance (g_syn) is shared within the synapses"
+        " between the source and target neuron sets.",
         json_schema_extra={
-            SchemaKey.UI_ELEMENT: UIElement.REFERENCE,
-            SchemaKey.REFERENCE_TYPE: AllDistributionsReference.__name__,
+            SchemaKey.UI_ELEMENT: UIElement.BOOLEAN_INPUT,
         },
     )
 
-    gsyn_distribution_shared_within: bool = Field(
+    fascilitation_time_shared_within: bool = Field(
         default=False,
-        title="g_syn Distribution Shared Within",
-        description="Whether the synaptic conductance (g_syn) is shared within the synapses"
+        title="Fascilitation Time Distribution Shared Within",
+        description="Whether the fascilitation time is shared within the synapses"
+        " between the source and target neuron sets.",
+        json_schema_extra={
+            SchemaKey.UI_ELEMENT: UIElement.BOOLEAN_INPUT,
+        },
+    )
+
+    depression_time_shared_within: bool = Field(
+        default=False,
+        title="Depression Time Distribution Shared Within",
+        description="Whether the depression time is shared within the synapses"
+        " between the source and target neuron sets.",
+        json_schema_extra={
+            SchemaKey.UI_ELEMENT: UIElement.BOOLEAN_INPUT,
+        },
+    )
+
+    n_rrp_vesicles_shared_within: bool = Field(
+        default=False,
+        title="Number of RRP Vesicles Distribution Shared Within",
+        description="Whether the number of RRP vesicles is shared within the synapses"
+        " between the source and target neuron sets.",
+        json_schema_extra={
+            SchemaKey.UI_ELEMENT: UIElement.BOOLEAN_INPUT,
+        },
+    )
+
+    decay_time_shared_within: bool = Field(
+        default=False,
+        title="Decay Time Distribution Shared Within",
+        description="Whether the decay time is shared within the synapses"
         " between the source and target neuron sets.",
         json_schema_extra={
             SchemaKey.UI_ELEMENT: UIElement.BOOLEAN_INPUT,
@@ -67,15 +156,40 @@ class TsodyksMarkramSynapticModel(Block):
         d["shared_within"] = self.u_hill_coefficient_shared_within
         return d
 
-    def gsyn_dict(self) -> dict:
-        d = self.gsyn_distribution.resolve()
-        d["shared_within"] = self.gsyn_distribution_shared_within
+    def conductance_distribution_dict(self) -> dict:
+        d = self.conductance_distribution.resolve()
+        d["shared_within"] = self.conductance_distribution_shared_within
         return d
+    
+    def fascilitation_time_dict(self) -> dict:
+        d = self.fascilitation_time.resolve()
+        d["shared_within"] = self.fascilitation_time_shared_within
+        return d
+    
+    def depression_time_dict(self) -> dict:
+        d = self.depression_time.resolve()
+        d["shared_within"] = self.depression_time_shared_within
+        return d
+    
+    def n_rrp_vesicles_dict(self) -> dict:
+        d = self.n_rrp_vesicles_distribution.resolve()
+        d["shared_within"] = self.n_rrp_vesicles_shared_within
+        return d
+    
+    def decay_time_dict(self) -> dict:
+        d = self.decay_time.resolve()
+        d["shared_within"] = self.decay_time_shared_within
+        return d
+
 
     def parameter_dictionaries(self) -> dict:
         return {
             "u_hill_coefficient": self.u_hill_coefficient_dict(),
-            "gsyn": self.gsyn_dict(),
+            "conductance": self.conductance_distribution_dict(),
+            "fascilitation_time": self.fascilitation_time_dict(),
+            "depression_time": self.depression_time_dict(),
+            "n_rrp_vesicles": self.n_rrp_vesicles_dict(),
+            "decay_time": self.decay_time_dict(),
         }
 
 
