9025452a2f574f0042d14bd3e85be918701d66d2
James Isbister <isbisterjb@gmail.com>
2026-03-23 15:29:13 +0100

    Working changes

--- obi_one/scientific/blocks/synaptic_parameterization/tsodyks_markram.py
+++ obi_one/scientific/blocks/synaptic_parameterization/tsodyks_markram.py
@@ -1,4 +1,6 @@
 import logging
+import pandas import pd
+import h5py
 from typing import Annotated, Never
 
 import bluepysnap as snap
@@ -86,38 +88,104 @@ class TsodyksMarkramSynapseParameterization(SynapseParameterization):
     @property
     def cov_dict(self) -> dict:
         return {}
+    
+    def _wrap_get_model_output(
+        self, cls_src: pd.Series, cls_tgt: pd.Series, row: pd.Series
+    ) -> pd.DataFrame:
+        src_type = cls_src[row["source"]]
+        tgt_type = cls_tgt[row["target"]]
+        idx = row["index"]
+
+        mdl = self._pathway_model.get_model_output(
+            src_type=src_type, tgt_type=tgt_type, n_syn=len(idx)
+        )
+        mdl["_index"] = idx
+        return mdl
+    
+    def _parameterize_edge_file(self, edge: snap.edges.EdgePopulation) -> None:
+    #     # Get pathway source/target values
+    #     pathway_property = self.pathway_property
+    #     if pathway_property not in edge.source.property_names:
+    #         msg = (
+    #             f"Pathway property '{pathway_property}' not found in source nodes:"
+    #             f" Skipping edge population '{edge.name}'!"
+    #         )
+    #         L.warning(msg)
+    #         return
+    #     if pathway_property not in edge.target.property_names:
+    #         msg = (
+    #             f"Pathway property '{pathway_property}' not found in target nodes:"
+    #             f" Skipping edge population '{edge.name}'!"
+    #         )
+    #         L.warning(msg)
+    #         return
+    #     cls_src = edge.source.get(properties=pathway_property)
+    #     cls_tgt = edge.target.get(properties=pathway_property)
+
+        # Open edge file
+        edge_prefix = f"edges/{edge.name}"
+        with h5py.File(edge.h5_filepath, "a") as h5:
+            edge_grp = h5[edge_prefix]
+
+            # Get connectivity
+            src_ids = edge_grp["source_node_id"]
+            tgt_ids = edge_grp["target_node_id"]
+            src_tgt_df = pd.DataFrame(
+                {"source": src_ids, "target": tgt_ids, "index": range(len(src_ids))}
+            )
+            src_tgt_df = src_tgt_df.groupby(["source", "target"])["index"].apply(list).reset_index()
+
+            # Draw values
+            drawn_values = [
+                self._wrap_get_model_output(cls_src, cls_tgt, src_tgt_df.iloc[i])
+                for i in range(len(src_tgt_df))
+            ]
+            new_props = pd.concat(drawn_values, axis=0).set_index("_index", drop=True).sort_index()
+            for col in new_props.columns:
+                new_values = new_props[col].to_numpy()
+                if col in edge_grp["0"]:
+                    msg = (
+                        f"Synapse property '{col}' already exists in edge population"
+                        f" '{edge.name}': "
+                    )
+                    if self.overwrite_if_exists:
+                        msg += "Re-parameterizing synapses."
+                        L.info(msg)
+                        edge_grp["0"][col][...] = new_values
+                    else:
+                        msg += "Choose 'overwrite' to re-parameterize synapses!"
+                        raise ValueError(msg)
+                else:
+                    edge_grp["0"].create_dataset(col, data=new_values)
+
+    def u_hill_coefficient_dict(self) -> dict:
+        d = self.u_hill_coefficient_distribution.resolve()
+        d["shared_within"] = self.u_hill_coefficient_shared_within
+        return d
+    
+    def gsyn_dict(self) -> dict:
+        d = self.gsyn_distribution.resolve()
+        d["shared_within"] = self.gsyn_distribution_shared_within
+        return d
+
 
     def go_for_it(self, circ: snap.Circuit) -> Never:
         source_node_set = self.source_neuron_set.resolve(circ)
         target_node_set = self.target_neuron_set.resolve(circ)
 
-        stats_dict = {
-            "u_hill_coefficient": {
-                source_node_set: {
-                    target_node_set: {
-                        "type": "gamma",
-                        "mean": 1.0,
-                        "std": 0.5,
-                        "shared_within": self.u_hill_coefficient_shared_within,
-                    }
-                }
-            },
-            "gsyn": {
-                source_node_set: {
-                    target_node_set: {
-                        "type": "gamma",
-                        "mean": 0.5,
-                        "std": 0.25,
-                        "shared_within": self.gsyn_distribution_shared_within,
-                    }
-                }
-            },
+        prop_stats = {
+            "u_hill_coefficient": {source_node_set: {
+                    target_node_set: self.u_hill_coefficient_dict()
+                }},
+            "gsyn": {source_node_set: {
+                    target_node_set: self.gsyn_dict()
+                }},
         }
 
         model1 = model_types.ConnPropsModel(
             src_types=[source_node_set],
             tgt_types=[target_node_set],
-            prop_stats=stats_dict,
+            prop_stats=prop_stats,
             prop_cov=self.cov_dict,
         )
 
