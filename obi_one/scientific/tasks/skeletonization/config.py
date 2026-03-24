a1a7d36cdca0e5722e872310cb7018e21bf2ba10
James Isbister <isbisterjb@gmail.com>
2026-03-24 20:44:40 +0100

    Update skeletonization to use TaskActivity and TaskConfig

--- obi_one/scientific/tasks/skeletonization/config.py
+++ obi_one/scientific/tasks/skeletonization/config.py
@@ -1,13 +1,11 @@
 import abc
 import logging
-from datetime import UTC, datetime
 from enum import StrEnum
-from pathlib import Path
 from typing import Annotated, ClassVar
 
-import entitysdk
-from entitysdk import models
-from entitysdk.types import AssetLabel, ContentType
+from entitysdk.client import Client
+from entitysdk.models import Entity
+from entitysdk.types import TaskActivityType, TaskConfigType
 from pydantic import ConfigDict, Field, PositiveFloat
 
 from obi_one.core.block import Block
@@ -16,7 +14,6 @@ from obi_one.core.schema import SchemaKey, UIElement
 from obi_one.core.single import SingleConfigMixin
 from obi_one.core.units import Units
 from obi_one.scientific.from_id.em_cell_mesh_from_id import EMCellMeshFromID
-from obi_one.scientific.library.constants import _COORDINATE_CONFIG_FILENAME, _SCAN_CONFIG_FILENAME
 from obi_one.scientific.library.info_scan_config.config import InfoScanConfig
 
 L = logging.getLogger(__name__)
@@ -35,8 +32,6 @@ class SkeletonizationScanConfig(InfoScanConfig, abc.ABC):
     name: ClassVar[str] = "Skeletonization Campaign"
     description: ClassVar[str] = "Skeletonization campaign"
 
-    _campaign: entitysdk.models.SkeletonizationCampaign = None
-
     model_config = ConfigDict(
         json_schema_extra={
             SchemaKey.UI_ENABLED: True,
@@ -45,6 +40,28 @@ class SkeletonizationScanConfig(InfoScanConfig, abc.ABC):
             ],
         }
     )
+
+    _campaign_task_config_type: ClassVar[TaskConfigType] = TaskConfigType.skeletonization__campaign
+    _campaign_generation_task_activity_type: ClassVar[TaskActivityType] = (
+        TaskActivityType.skeletonization__config_generation
+    )
+
+    def input_entities(self, db_client: Client) -> list[Entity]:
+        L.info("-- Register SkeletonizationCampaign Entity")
+        if isinstance(
+            self.initialize.cell_mesh,
+            EMCellMeshFromID,
+        ):
+            input_meshes = [self.initialize.cell_mesh.entity(db_client)]
+
+        elif isinstance(self.initialize.cell_mesh, list):
+            if len(self.initialize.cell_mesh) > 0:
+                input_meshes = [mesh.entity(db_client) for mesh in self.initialize.cell_mesh]
+            else:
+                msg = "No cell meshes provided for skeletonization campaign!"
+                raise OBIONEError(msg)
+
+        return input_meshes
 
     class Initialize(Block):
         cell_mesh: EMCellMeshFromID | list[EMCellMeshFromID] = Field(
@@ -89,95 +106,9 @@ class SkeletonizationScanConfig(InfoScanConfig, abc.ABC):
         },
     )
 
-    def create_campaign_entity_with_config(
-        self,
-        output_root: Path,
-        multiple_value_parameters_dictionary: dict | None = None,
-        db_client: entitysdk.client.Client = None,
-    ) -> entitysdk.models.SkeletonizationCampaign:
-        """Initializes the simulation campaign in the database."""
-        L.info("1. Initializing skeletonization campaign in the database...")
-
-        if multiple_value_parameters_dictionary is None:
-            multiple_value_parameters_dictionary = {}
-
-        L.info("-- Register SkeletonizationCampaign Entity")
-        if isinstance(
-            self.initialize.cell_mesh,
-            EMCellMeshFromID,
-        ):
-            input_meshes = [self.initialize.cell_mesh.entity(db_client)]
-
-        elif isinstance(self.initialize.cell_mesh, list):
-            if len(self.initialize.cell_mesh) > 0:
-                input_meshes = [mesh.entity(db_client) for mesh in self.initialize.cell_mesh]
-            else:
-                msg = "No cell meshes provided for skeletonization campaign!"
-                raise OBIONEError(msg)
-
-        self._campaign = db_client.register_entity(
-            entitysdk.models.SkeletonizationCampaign(
-                name=self.info.campaign_name,
-                description=self.info.campaign_description,
-                input_meshes=input_meshes,
-                scan_parameters=multiple_value_parameters_dictionary,
-            )
-        )
-
-        L.info("-- Upload campaign_generation_config")
-        _ = db_client.upload_file(
-            entity_id=self._campaign.id,
-            entity_type=entitysdk.models.SkeletonizationCampaign,
-            file_path=output_root / _SCAN_CONFIG_FILENAME,
-            file_content_type=ContentType.application_json,
-            asset_label=AssetLabel.campaign_generation_config,
-        )
-
-        return self._campaign
-
-    def create_campaign_generation_entity(
-        self,
-        skeletonization_configs: list[entitysdk.models.SkeletonizationConfig],
-        db_client: entitysdk.client.Client,
-    ) -> None:
-        L.info("3. Saving completed skeletonization campaign generation")
-
-        L.info("-- Register SkeletonizationConfigGeneration Entity")
-        db_client.register_entity(
-            entitysdk.models.SkeletonizationConfigGeneration(
-                start_time=datetime.now(UTC),
-                used=[self._campaign],
-                generated=skeletonization_configs,
-            )
-        )
-
 
 class SkeletonizationSingleConfig(SkeletonizationScanConfig, SingleConfigMixin):
-    def create_single_entity_with_config(
-        self, campaign: entitysdk.models.SkeletonizationCampaign, db_client: entitysdk.client.Client
-    ) -> entitysdk.models.SkeletonizationConfig:
-        """Saves the SkeletonizationConfig to the database."""
-        L.info(f"2.{self.idx} Saving SkeletonizationConfig {self.idx} to database...")
-
-        L.info("-- Register SkeletonizationConfig Entity")
-        self._single_entity = db_client.register_entity(
-            entitysdk.models.SkeletonizationConfig(
-                name=f"SkeletonizationConfig {self.idx}",
-                description=f"SkeletonizationConfig {self.idx}",
-                scan_parameters=self.single_coordinate_scan_params.dictionary_representaiton(),
-                skeletonization_campaign_id=campaign.id,
-                em_cell_mesh_id=self.initialize.cell_mesh.id_str,
-            )
-        )
-
-        L.info("-- Upload skeltonization_config asset")
-        L.info(Path(self.coordinate_output_root, _COORDINATE_CONFIG_FILENAME))
-        L.info(self.single_entity)
-        L.info(self.single_entity.id)
-        _ = db_client.upload_file(
-            entity_id=self.single_entity.id,
-            entity_type=models.SkeletonizationConfig,
-            file_path=Path(self.coordinate_output_root, _COORDINATE_CONFIG_FILENAME),
-            file_content_type=ContentType.application_json,
-            asset_label=AssetLabel.skeletonization_config,
-        )
+    _single_task_config_type: ClassVar[TaskConfigType] = TaskConfigType.skeletonization__config
+    _single_task_activity_type: ClassVar[TaskActivityType] = (
+        TaskActivityType.skeletonization__execution
+    )
