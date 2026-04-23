0ed973491a88cb092206eca0e9307c615072f417
g-bar <gbarv2@gmail.com>
2026-04-23 15:53:38 +0200

    Simplify morphology data for circuit viz (#716)
    Thanks!

--- app/schemas/circuit_visualization.py
+++ app/schemas/circuit_visualization.py
@@ -1,7 +1,8 @@
+from enum import IntEnum
 from pathlib import Path
-from typing import Annotated, Literal
+from typing import Annotated, Literal, TypedDict
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, RootModel
 
 
 class Node(BaseModel):
@@ -15,47 +16,43 @@ class Node(BaseModel):
     orientation: Annotated[
         tuple[float, float, float, float], Field(description="Orientation quaternion (x, y, z, w)")
     ]
-    soma_radius: float | None
 
 
 Nodes = list[Node]
 
 
-class NeuronSectionInfo(BaseModel):
-    """Follows the interface defined in
-    https://github.com/openbraininstitute/core-web-app/blob/3c09493d1b786aa2c7573305e4410841ef269e05/src/services/bluenaas-single-cell/types.ts.
-    """
-
-    index: int
-    name: str
-    nseg: int
-    distance_from_soma: float
-    sec_length: float
-    xstart: list[float]
-    xend: list[float]
-    xcenter: list[float]
-    xdirection: list[float]
-    ystart: list[float]
-    yend: list[float]
-    ycenter: list[float]
-    ydirection: list[float]
-    zstart: list[float]
-    zend: list[float]
-    zcenter: list[float]
-    zdirection: list[float]
-    diam: list[float]
-    length: list[float]
-    distance: list[float]
-    segment_distance_from_soma: list[float]
-    segx: list[float]
-    neuron_section_id: int
-    neuron_segments_offset: list[int]
-    parent_index: int
-
-
-Morphology = dict[str, NeuronSectionInfo]
-
-
 class MorphPath(BaseModel):
     path: Path
     format: Literal["asc", "h5", "swc"]
+
+
+class MorphoViewerTreeItemType(IntEnum):
+    Soma = 0
+    Dendrite = 1
+    BasalDendrite = 2
+    ApicalDendrite = 3
+    Myelin = 4
+    Axon = 5
+    Selected = 6
+    Liaison = 7
+    Unknown = 8
+
+
+class Section(BaseModel):
+    id: str
+    parent_id: str | None
+    type: MorphoViewerTreeItemType
+    points: list[tuple[float, float, float]]
+    radii: list[float]
+
+
+class SectionDict(TypedDict):
+    id: str
+    parent_id: str | None
+    type: MorphoViewerTreeItemType
+    points: list[tuple[float, float, float]]
+    radii: list[float]
+
+
+class Sections(RootModel[list[Section]]):
+    pass
