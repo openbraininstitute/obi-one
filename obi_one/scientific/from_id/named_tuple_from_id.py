27755eb09355a418c8946f6a1342da0d63ddac4a
Aurélien <72930209+AurelienJaquier@users.noreply.github.com>
2026-04-29 16:38:51 +0200

    lint fix

--- obi_one/scientific/from_id/named_tuple_from_id.py
+++ obi_one/scientific/from_id/named_tuple_from_id.py
@@ -1,4 +1,3 @@
-
 from pydantic import Field
 
 from obi_one.core.tuple import NamedTuple
@@ -7,15 +6,12 @@ from obi_one.scientific.from_id.memodel_from_id import MEModelFromID
 
 
 class CellMorphologyNamedTuple(NamedTuple):
-
     elements: tuple[CellMorphologyFromID, ...] = Field(min_length=1)
 
 
 class MEModelNamedTuple(NamedTuple):
-
     elements: tuple[MEModelFromID, ...] = Field(min_length=1)
 
+
 class CellMorphologyAndMEModelNamedTuple(NamedTuple):
-
     elements: tuple[CellMorphologyFromID | MEModelFromID, ...] = Field(min_length=1)
-
