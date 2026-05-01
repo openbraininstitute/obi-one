cc65618c745b68ae860478434048b2b1a5656035
Christoph Pokorny <christoph.pokorny@openbraininstitute.org>
2026-05-01 17:37:36 +0200

    Initial commit of unified circuit registration module

---
+++ obi_one/utils/circuit_registration.py
@@ -0,0 +1,8 @@
+"""Utilities for registering circuit entities to entitycore."""
+
+import logging
+
+from entitysdk import Client, models
+from entitysdk.types import DerivationType
+
+L = logging.getLogger(__name__)
