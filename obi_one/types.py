3132d095e1eed3f523c42dab75f11b08210abed2
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2026-05-11 14:56:57 +0200

    Add flywire simulation task type

--- obi_one/types.py
+++ obi_one/types.py
@@ -19,6 +19,7 @@ class TaskType(StrEnum):
     circuit_simulation_inait_machine = auto()
     circuit_simulation_neuron = auto()
     circuit_simulation_neurodamus_cluster = auto()
+    circuit_simulation_brian2_machine = auto()
     morphology_skeletonization = auto()
     ion_channel_model_simulation_execution = auto()
     em_synapse_mapping = auto()
