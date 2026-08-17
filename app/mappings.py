f92f157dc6b623b3753303970acead1e6032fec4
Christoph Pokorny <christoph.pokorny@openbraininstitute.org>
2026-08-17 14:24:21 +0200

    Updated mapping

--- app/mappings.py
+++ app/mappings.py
@@ -19,7 +19,7 @@ from app.types import BuiltinScript, MachineExecutorImageType, TaskType
 from obi_one.config import settings as obi_settings
 
 # APP_TAG = f"tag:{(settings.APP_VERSION or '0.0.0').split('-')[0]}"
-APP_TAG = "commit:5fa467e84e09271ff0a82e98d0dbfac83f868878"
+APP_TAG = "commit:035a1a56c7ad74572bc26d3e871202dc1df9d526"
 OBI_ONE_CODE_PATH = str(Path(settings.OBI_ONE_LAUNCH_PATH) / "main.py")
 OBI_ONE_DEPS_DIR = Path(settings.OBI_ONE_LAUNCH_PATH) / "dependencies"
 
