11426cd0e26119680f2a9e8a61ec9b710c9384c7
Christoph Pokorny <christoph.pokorny@openbraininstitute.org>
2026-09-14 15:24:17 +0200

    Updated mapping

--- app/mappings.py
+++ app/mappings.py
@@ -210,7 +210,7 @@ TASK_DEFINITIONS: dict[TaskType, TaskDefinition] = {
         code=PythonRepositoryCode(
             location=settings.OBI_ONE_REPO,
             # ref=APP_TAG,
-            ref="commit:ba1442d83a9a3db4e5e204097cc31c8883ecd66b",  # TESTING
+            ref="commit:0d2c2431e6fb3149841eb9b140671f4cca0e2f2d",  # TESTING
             path=OBI_ONE_CODE_PATH,
             dependencies=str(OBI_ONE_DEPS_DIR / "synapse_parameterization.txt"),
         ),
